97d68a6b31570cc13408937cd738d36eaf8888dc
vit9696 <vit9696@users.noreply.github.com>
2018-10-24 18:04:33 +0300

    Fix OcTimerLib interface to be compatible with TimerLib.h

--- Library/OcTimerLib/OcTimerLib.c
+++ Library/OcTimerLib/OcTimerLib.c
@@ -22,21 +22,19 @@
 #include <Library/IoLib.h>
 #include <Library/PciLib.h>
 #include <Library/UefiBootServicesTableLib.h>
+#include <Library/TimerLib.h>
 
 #include <Macros.h>
 
-UINT64 mTimerLibTscFrequency = 0;
+STATIC UINT64 mPerformanceCounterFrequency = 0;
 
-// CalculateTSC
 /** Calculate the TSC frequency
-
-  @param[in] Recalculate  The firmware allocated handle for the EFI image.
 
   @retval  The calculated TSC frequency.
 **/
 UINT64
-CalculateTSC (
-  IN BOOLEAN  Recalculate
+RecalculateTSC (
+  VOID
   )
 {
   UINT32 TimerAddr;
@@ -50,7 +48,7 @@ CalculateTSC (
 
   // Already Calculated, Return Cached Result
 
-  if (((mTimerLibTscFrequency == 0) || Recalculate) && PciRead16 (PCI_ICH_LPC_ADDRESS (0)) == 0x8086) {
+  if (PciRead16 (PCI_ICH_LPC_ADDRESS (0)) == 0x8086) {
     TimerAddr       = 0;
     TimerResolution = 10;
 
@@ -73,7 +71,7 @@ CalculateTSC (
     }
 
     if (TimerAddr != 0) {
-      mTimerLibTscFrequency = 0;
+      mPerformanceCounterFrequency = 0;
 
       // Check That Timer Is Advancing
 
@@ -121,14 +119,172 @@ CalculateTSC (
           // Keep Checking Acpi ticks Until Target Is Reached
         } while (AcpiTicksDelta < AcpiTicksTarget);
 
-        Tsc1                  = AsmReadTsc ();
-        mTimerLibTscFrequency = MultU64x32 ((Tsc1 - Tsc0), TimerResolution);
+        Tsc1                         = AsmReadTsc ();
+        mPerformanceCounterFrequency = MultU64x32 ((Tsc1 - Tsc0), TimerResolution);
 
       }
     }
   }
 
-  //DEBUG ((DEBUG_INFO, "TscFrequency %lld\n", mTimerLibTscFrequency));
+  //DEBUG ((DEBUG_INFO, "TscFrequency %lld\n", mPerformanceCounterFrequency));
 
-  return mTimerLibTscFrequency;
+  return mPerformanceCounterFrequency;
+}
+
+UINTN
+EFIAPI
+MicroSecondDelay (
+  IN      UINTN                     MicroSeconds
+  )
+{
+  (VOID) MicroSeconds;
+  return EFI_UNSUPPORTED;
+}
+
+/**
+  Stalls the CPU for at least the given number of nanoseconds.
+
+  Stalls the CPU for the number of nanoseconds specified by NanoSeconds.
+
+  @param  NanoSeconds The minimum number of nanoseconds to delay.
+
+  @return The value of NanoSeconds inputted.
+
+**/
+UINTN
+EFIAPI
+NanoSecondDelay (
+  IN      UINTN                     NanoSeconds
+  )
+{
+  (VOID) NanoSeconds;
+  return EFI_UNSUPPORTED;
+}
+
+/**
+  Retrieves the current value of a 64-bit free running performance counter.
+
+  The counter can either count up by 1 or count down by 1. If the physical
+  performance counter counts by a larger increment, then the counter values
+  must be translated. The properties of the counter can be retrieved from
+  GetPerformanceCounterProperties().
+
+  @return The current value of the free running performance counter.
+
+**/
+UINT64
+EFIAPI
+GetPerformanceCounter (
+  VOID
+  )
+{
+  return AsmReadTsc ();
+}
+
+/**
+  Retrieves the 64-bit frequency in Hz and the range of performance counter
+  values.
+
+  If StartValue is not NULL, then the value that the performance counter starts
+  with immediately after is it rolls over is returned in StartValue. If
+  EndValue is not NULL, then the value that the performance counter end with
+  immediately before it rolls over is returned in EndValue. The 64-bit
+  frequency of the performance counter in Hz is always returned. If StartValue
+  is less than EndValue, then the performance counter counts up. If StartValue
+  is greater than EndValue, then the performance counter counts down. For
+  example, a 64-bit free running counter that counts up would have a StartValue
+  of 0 and an EndValue of 0xFFFFFFFFFFFFFFFF. A 24-bit free running counter
+  that counts down would have a StartValue of 0xFFFFFF and an EndValue of 0.
+
+  @param  StartValue  The value the performance counter starts with when it
+                      rolls over.
+  @param  EndValue    The value that the performance counter ends with before
+                      it rolls over.
+
+  @return The frequency in Hz.
+
+**/
+UINT64
+EFIAPI
+GetPerformanceCounterProperties (
+  OUT      UINT64                    *StartValue,  OPTIONAL
+  OUT      UINT64                    *EndValue     OPTIONAL
+  )
+{
+  if (StartValue != NULL) {
+    *StartValue = 0;
+  }
+
+  if (EndValue != NULL) {
+    *EndValue = 0xffffffffffffffffULL;
+  }
+  return mPerformanceCounterFrequency;
+}
+
+/**
+  Converts elapsed ticks of performance counter to time in nanoseconds.
+
+  This function converts the elapsed ticks of running performance counter to
+  time value in unit of nanoseconds.
+
+  @param  Ticks     The number of elapsed ticks of running performance counter.
+
+  @return The elapsed time in nanoseconds.
+
+**/
+UINT64
+EFIAPI
+GetTimeInNanoSecond (
+  IN UINT64  Ticks
+  )
+{
+  UINT64  Frequency;
+  UINT64  NanoSeconds;
+  UINT64  Remainder;
+  INTN    Shift;
+
+  Frequency = GetPerformanceCounterProperties (NULL, NULL);
+
+  if (Frequency == 0) {
+    return 0;
+  }
+
+  //
+  //          Ticks
+  // Time = --------- x 1,000,000,000
+  //        Frequency
+  //
+  NanoSeconds = MultU64x32 (DivU64x64Remainder (Ticks, Frequency, &Remainder), 1000000000u);
+
+  //
+  // Ensure (Remainder * 1,000,000,000) will not overflow 64-bit.
+  // Since 2^29 < 1,000,000,000 = 0x3B9ACA00 < 2^30, Remainder should < 2^(64-30) = 2^34,
+  // i.e. highest bit set in Remainder should <= 33.
+  //
+  Shift = MAX (0, HighBitSet64 (Remainder) - 33);
+  Remainder = RShiftU64 (Remainder, (UINTN) Shift);
+  Frequency = RShiftU64 (Frequency, (UINTN) Shift);
+  NanoSeconds += DivU64x64Remainder (MultU64x32 (Remainder, 1000000000u), Frequency, NULL);
+
+  return NanoSeconds;
+}
+
+/**
+  The constructor function caches PerformanceCounterFrequency.
+
+  @param  ImageHandle   The firmware allocated handle for the EFI image.
+  @param  SystemTable   A pointer to the EFI System Table.
+
+  @retval EFI_SUCCESS   The constructor always returns RETURN_SUCCESS.
+
+**/
+RETURN_STATUS
+EFIAPI
+OcTimerLibConstructor (
+  VOID
+  )
+{
+  RecalculateTSC ();
+
+  return EFI_SUCCESS;
 }
