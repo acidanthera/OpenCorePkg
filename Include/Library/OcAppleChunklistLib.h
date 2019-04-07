c1e462f277057647d6df8f997e0620e5694d393a
Download-Fritz <download-fritz@outlook.com>
2019-04-07 12:53:37 +0200

    OcAppleChunklistLib: Rework function prototypes and Context

--- Include/Library/OcAppleChunklistLib.h
+++ Include/Library/OcAppleChunklistLib.h
@@ -21,11 +21,10 @@
 // Chunklist context.
 //
 typedef struct OC_APPLE_CHUNKLIST_CONTEXT_ {
-  APPLE_CHUNKLIST_HEADER  *Header;
-  UINTN                   FileSize;
-  APPLE_CHUNKLIST_CHUNK   *Chunks;
-  APPLE_CHUNKLIST_SIG     *Signature;
-  UINT8                   Hash[SHA256_DIGEST_SIZE];
+  UINT64                      ChunkCount;
+  CONST APPLE_CHUNKLIST_CHUNK *Chunks;
+  APPLE_CHUNKLIST_SIG         *Signature;
+  UINT8                       Hash[SHA256_DIGEST_SIZE];
 } OC_APPLE_CHUNKLIST_CONTEXT;
 
 //
@@ -35,20 +34,19 @@ typedef struct OC_APPLE_CHUNKLIST_CONTEXT_ {
 /**
   Initializes a chunklist context.
 
-  @param[in]  Buffer            A pointer to a buffer containing the chunklist data.
-  @param[in]  Length            The length of the buffer specified in Buffer.
   @param[out] Context           The Context to initialize.
+  @param[in]  Buffer            A pointer to a buffer containing the chunklist data.
+  @param[in]  BufferSize        The size of the buffer specified in Buffer.
 
   @retval EFI_SUCCESS           The Context was intialized successfully.
   @retval EFI_INVALID_PARAMETER One or more parameters are invalid.
   @retval EFI_UNSUPPORTED       The chunklist is unsupported.
 **/
-EFI_STATUS
-EFIAPI
+BOOLEAN
 OcAppleChunklistInitializeContext (
+  OUT OC_APPLE_CHUNKLIST_CONTEXT  *Context,
   IN  VOID                        *Buffer,
-  IN  UINTN                       Length,
-  OUT OC_APPLE_CHUNKLIST_CONTEXT  *Context
+  IN  UINT32                      BufferSize
   );
 
 BOOLEAN
@@ -62,19 +60,18 @@ OcAppleChunklistVerifySignature (
 
   @param[in] Context            The Context to verify against.
   @param[in] Buffer             A pointer to a buffer containing the data to be verified.
-  @param[in] Length             The length of the buffer specified in Buffer.
+  @param[in] BufferSize         The size of the buffer specified in Buffer.
 
   @retval EFI_SUCCESS           The data was verified successfully.
   @retval EFI_INVALID_PARAMETER One or more parameters are invalid.
   @retval EFI_END_OF_FILE       The end of Buffer was reached.
   @retval EFI_COMPROMISED_DATA  The data failed verification.
 **/
-EFI_STATUS
-EFIAPI
+BOOLEAN
 OcAppleChunklistVerifyData (
-  IN OC_APPLE_CHUNKLIST_CONTEXT *Context,
-  IN VOID                       *Buffer,
-  IN UINTN                      Length
+  IN OUT OC_APPLE_CHUNKLIST_CONTEXT  *Context,
+  IN     VOID                        *Buffer,
+  IN     UINTN                       BufferSize
   );
 
 #endif // APPLE_CHUNKLIST_LIB_H
--- Library/OcAppleChunklistLib/OcAppleChunklistLib.c
+++ Library/OcAppleChunklistLib/OcAppleChunklistLib.c
@@ -10,21 +10,19 @@
   WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 **/
 
-#include <Uefi.h>
-#include <Library/BaseLib.h>
+#include <Base.h>
+
 #include <Library/BaseMemoryLib.h>
 #include <Library/DebugLib.h>
 #include <Library/OcAppleChunklistLib.h>
 #include <Library/OcCryptoLib.h>
 #include <Library/OcGuardLib.h>
-#include <Library/UefiLib.h>
 
-EFI_STATUS
-EFIAPI
+BOOLEAN
 OcAppleChunklistInitializeContext (
+  OUT OC_APPLE_CHUNKLIST_CONTEXT  *Context,
   IN  VOID                        *Buffer,
-  IN  UINTN                       Length,
-  OUT OC_APPLE_CHUNKLIST_CONTEXT  *Context
+  IN  UINT32                      BufferSize
   )
 {
   APPLE_CHUNKLIST_HEADER  *ChunklistHeader;
@@ -35,13 +33,13 @@ OcAppleChunklistInitializeContext (
   UINT8                   SwapValue;
 
   ASSERT (Buffer != NULL);
-  ASSERT (Length > 0);
+  ASSERT (BufferSize > 0);
   ASSERT (Context != NULL);
 
   ChunklistHeader = (APPLE_CHUNKLIST_HEADER *) Buffer;
 
-  if (Length < sizeof (APPLE_CHUNKLIST_HEADER)) {
-    return EFI_INVALID_PARAMETER;
+  if (BufferSize < sizeof (APPLE_CHUNKLIST_HEADER)) {
+    return FALSE;
   }
 
   //
@@ -52,29 +50,27 @@ OcAppleChunklistInitializeContext (
     || ChunklistHeader->FileVersion != APPLE_CHUNKLIST_FILE_VERSION_10
     || ChunklistHeader->ChunkMethod != APPLE_CHUNKLIST_CHUNK_METHOD_10
     || ChunklistHeader->SigMethod != APPLE_CHUNKLIST_SIG_METHOD_10) {
-    return EFI_UNSUPPORTED;
+    return FALSE;
   }
 
-  ZeroMem (Context, sizeof (OC_APPLE_CHUNKLIST_CONTEXT));
-  Context->Header    = ChunklistHeader;
-  Context->FileSize  = Length;
+  Context->ChunkCount = ChunklistHeader->ChunkCount;
 
   //
   // Ensure that chunk and signature addresses are valid in the first place.
   //
   if (OcOverflowAddUN ((UINTN) Buffer, (UINTN) ChunklistHeader->ChunkOffset, (UINTN *) &Context->Chunks)
     || OcOverflowAddUN ((UINTN) Buffer, (UINTN) ChunklistHeader->SigOffset, (UINTN *) &Context->Signature)) {
-    return EFI_INVALID_PARAMETER;
+    return FALSE;
   }
 
   //
   // Ensure that chunks and signature reside within Buffer.
   //
   if (OcOverflowMulAddUN (sizeof (APPLE_CHUNKLIST_CHUNK), ChunklistHeader->ChunkCount, (UINTN) Context->Chunks, &DataEnd)
-    || DataEnd > (UINTN) Buffer + Length
+    || DataEnd > (UINTN) Buffer + BufferSize
     || OcOverflowAddUN (sizeof (APPLE_CHUNKLIST_SIG), (UINTN) Context->Signature, &DataEnd)
-    || DataEnd != (UINTN) Buffer + Length) {
-    return EFI_INVALID_PARAMETER;
+    || DataEnd != (UINTN) Buffer + BufferSize) {
+    return FALSE;
   }
 
   //
@@ -91,13 +87,13 @@ OcAppleChunklistInitializeContext (
     Signature[SigLength - Index - 1] = SwapValue;
   }
 
-  return EFI_SUCCESS;
+  return TRUE;
 }
 
 BOOLEAN
 OcAppleChunklistVerifySignature (
-  IN OC_APPLE_CHUNKLIST_CONTEXT  *Context,
-  IN RSA_PUBLIC_KEY              *PublicKey
+  IN OUT OC_APPLE_CHUNKLIST_CONTEXT  *Context,
+  IN     RSA_PUBLIC_KEY              *PublicKey
   )
 {
   BOOLEAN Result;
@@ -122,36 +118,32 @@ OcAppleChunklistVerifySignature (
   return Result;
 }
 
-EFI_STATUS
-EFIAPI
+BOOLEAN
 OcAppleChunklistVerifyData (
-  IN OC_APPLE_CHUNKLIST_CONTEXT  *Context,
-  IN  VOID                       *Buffer,
-  IN  UINTN                      Length
+  IN OUT OC_APPLE_CHUNKLIST_CONTEXT  *Context,
+  IN     VOID                        *Buffer,
+  IN     UINTN                       BufferSize
   )
 {
-  UINT64                 Index;
-  UINTN                  RemainingLength;
-  UINT8                  *BufferCurrent;
-  UINT8                  ChunkHash[SHA256_DIGEST_SIZE];
-  UINT64                 ChunkCount;
-  APPLE_CHUNKLIST_CHUNK  *CurrentChunk;
+  UINT64                      Index;
+  UINTN                       RemainingLength;
+  UINT8                       *BufferCurrent;
+  UINT8                       ChunkHash[SHA256_DIGEST_SIZE];
+  UINT64                      ChunkCount;
+  CONST APPLE_CHUNKLIST_CHUNK *CurrentChunk;
 
   ASSERT (Context != NULL);
   ASSERT (Buffer != NULL);
-  ASSERT (Length > 0);
-  ASSERT (Context->Header != NULL);
-  ASSERT (Context->FileSize > 0);
+  ASSERT (BufferSize > 0);
   ASSERT (Context->Chunks != NULL);
-  ASSERT (Context->Signature != NULL);
 
   DEBUG_CODE (
     ASSERT (Context->Signature == NULL);
     );
 
-  RemainingLength = Length;
+  RemainingLength = BufferSize;
   BufferCurrent   = (UINT8 *) Buffer;
-  ChunkCount      = Context->Header->ChunkCount;
+  ChunkCount      = Context->ChunkCount;
   CurrentChunk    = &Context->Chunks[0];
 
   for (Index = 0; Index < ChunkCount; Index++) {
@@ -159,7 +151,7 @@ OcAppleChunklistVerifyData (
     // Ensure length of chunk is valid.
     //
     if (RemainingLength < CurrentChunk->Length) {
-      return EFI_END_OF_FILE;
+      return FALSE;
     }
 
     //
@@ -169,7 +161,7 @@ OcAppleChunklistVerifyData (
       Index, ChunkCount));
     Sha256 (ChunkHash, BufferCurrent, CurrentChunk->Length);
     if (CompareMem (ChunkHash, CurrentChunk->Checksum, SHA256_DIGEST_SIZE) != 0) {
-      return EFI_COMPROMISED_DATA;
+      return FALSE;
     }
 
     //
@@ -180,5 +172,5 @@ OcAppleChunklistVerifyData (
     CurrentChunk++;
   }
 
-  return EFI_SUCCESS;
+  return TRUE;
 }
