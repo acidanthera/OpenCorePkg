66abac936a3804af17208aadade08fff7f8337db
vit9696 <vit9696@users.noreply.github.com>
2019-04-09 23:46:47 +0300

    OcMiscLib: Release EHCI and UHCI ownership, based on work by Needy

--- Library/OcMiscLib/ReleaseUsbOwnership.c
+++ Library/OcMiscLib/ReleaseUsbOwnership.c
@@ -20,6 +20,7 @@
 
 #include <Library/BaseMemoryLib.h>
 #include <Library/DebugLib.h>
+#include <Library/IoLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 
 #define XHC_HCCPARAMS_OFFSET      0x10
@@ -28,6 +29,12 @@
 #define XHC_USBCMD_OFFSET         0x0    ///< USB Command Register Offset
 #define XHC_USBSTS_OFFSET         0x4    ///< USB Status Register Offset
 #define XHC_POLL_DELAY            1000
+
+#define EHC_BAR_INDEX             0x0
+#define EHC_HCCPARAMS_OFFSET      0x8
+#define EHC_USBCMD_OFFSET         0x0    ///< USB Command Register Offset
+#define EHC_USBSTS_OFFSET         0x4    ///< USB Status Register Offset
+#define EHC_USBINT_OFFSET         0x8    ///< USB Interrupt Enable Register
 
 /**
   Release XHCI USB controllers ownership.
@@ -183,6 +190,371 @@ XhciReleaseOwnership (
   return Status;
 }
 
+/**
+ Release EHCI USB controllers ownership.
+
+ @param[in]  PciIo  PCI I/O protocol for the device.
+
+ @retval EFI_NOT_FOUND  No EHCI controllers had ownership incorrectly set.
+ @retval EFI_SUCCESS    Corrected EHCI controllers ownership to OS.
+ **/
+STATIC
+EFI_STATUS
+EhciReleaseOwnership (
+  IN  EFI_PCI_IO_PROTOCOL   *PciIo
+  )
+{
+  EFI_STATUS       Status;
+  UINT32           Value;
+  UINT32           Base;
+  UINT32           OpAddr;
+  UINT32           ExtendCap;
+  UINT32           UsbCmd;
+  UINT32           UsbSts;
+  UINT32           UsbIntr;
+  UINT32           UsbLegSup;
+  UINT32           UsbLegCtlSts;
+  UINTN            IsOsOwned;
+  UINTN            IsBiosOwned;
+  BOOLEAN          IsOwnershipConflict;
+  UINT32           HcCapParams;
+  INT32            TimeOut;
+
+  Value = 0x0002;
+
+  PciIo->Pci.Write (
+    PciIo,
+    EfiPciIoWidthUint16,
+    0x04,
+    1,
+    &Value
+    );
+
+  Base = 0;
+
+  Status = PciIo->Pci.Read (
+    PciIo,
+    EfiPciIoWidthUint32,
+    0x10,
+    1,
+    &Base
+    );
+
+  if (MmioRead8 (Base) < 0x0C) {
+    //
+    // Config space too small: no legacy implementation.
+    //
+    return EFI_NOT_FOUND;
+  }
+
+  //
+  // Operational Registers = capaddr + offset (8bit CAPLENGTH in Capability Registers + offset 0).
+  //
+  OpAddr = Base + MmioRead8 (Base);
+
+  Status = PciIo->Mem.Read (
+    PciIo,
+    EfiPciIoWidthUint32,
+    EHC_BAR_INDEX,
+    EHC_HCCPARAMS_OFFSET,
+    1,
+    &HcCapParams
+    );
+
+  ExtendCap = (HcCapParams >> 8U) & 0xFFU;
+
+  UsbCmd  = MmioRead32 (OpAddr + EHC_USBCMD_OFFSET);
+  UsbSts  = MmioRead32 (OpAddr + EHC_USBSTS_OFFSET);
+  UsbIntr = MmioRead32 (OpAddr + EHC_USBINT_OFFSET);
+
+  //
+  // Read PCI Config 32bit USBLEGSUP (eecp+0).
+  //
+  Status = PciIo->Pci.Read (
+    PciIo,
+    EfiPciIoWidthUint32,
+    ExtendCap,
+    1,
+    &UsbLegSup
+    );
+
+  IsBiosOwned = (UsbLegSup & BIT16) != 0;
+  if (!IsBiosOwned) {
+    //
+    // No BIOS ownership, ignore.
+    //
+    return EFI_NOT_FOUND;
+  }
+
+  //
+  // Read PCI Config 32bit USBLEGCTLSTS (eecp+4).
+  //
+  PciIo->Pci.Read (
+    PciIo,
+    EfiPciIoWidthUint32,
+    ExtendCap + 0x4,
+    1,
+    &UsbLegCtlSts
+    );
+
+  //
+  // Disable the SMI in USBLEGCTLSTS firstly.
+  //
+  UsbLegCtlSts &= 0xFFFF0000U;
+  PciIo->Pci.Write (
+    PciIo,
+    EfiPciIoWidthUint32,
+    ExtendCap + 0x4,
+    1,
+    &UsbLegCtlSts
+    );
+
+  gBS->Stall (500);
+  UsbCmd  = MmioRead32 (OpAddr + EHC_USBCMD_OFFSET);
+  UsbSts  = MmioRead32 (OpAddr + EHC_USBSTS_OFFSET);
+  UsbIntr = MmioRead32 (OpAddr + EHC_USBINT_OFFSET);
+
+  //
+  // Clear registers to default.
+  //
+  UsbCmd = UsbCmd & 0xFFFFFF00U;
+  MmioWrite32 (OpAddr + EHC_USBCMD_OFFSET, UsbCmd);
+  MmioWrite32 (OpAddr + EHC_USBINT_OFFSET, 0);
+  MmioWrite32 (OpAddr + EHC_USBSTS_OFFSET, 0x1000);
+
+  Value = 1;
+  PciIo->Pci.Write (
+    PciIo,
+    EfiPciIoWidthUint32,
+    ExtendCap,
+    1,
+    &Value
+    );
+
+  //
+  // Get the results.
+  //
+  UsbCmd  = MmioRead32 (OpAddr + EHC_USBCMD_OFFSET);
+  UsbSts  = MmioRead32 (OpAddr + EHC_USBSTS_OFFSET);
+  UsbIntr = MmioRead32 (OpAddr + EHC_USBINT_OFFSET);
+
+  //
+  // Read 32bit USBLEGSUP (eecp+0).
+  //
+  PciIo->Pci.Read (
+    PciIo,
+    EfiPciIoWidthUint32,
+    ExtendCap,
+    1,
+    &UsbLegSup
+    );
+
+  IsBiosOwned = (UsbLegSup & BIT16) != 0;
+  IsOsOwned   = (UsbLegSup & BIT24) != 0;
+
+  //
+  // Read 32bit USBLEGCTLSTS (eecp+4).
+  //
+  PciIo->Pci.Read (
+    PciIo,
+    EfiPciIoWidthUint32,
+    ExtendCap + 0x4,
+    1,
+    &UsbLegCtlSts
+    );
+
+  //
+  // Get EHCI Ownership from legacy bios.
+  //
+  PciIo->Pci.Read (
+    PciIo,
+    EfiPciIoWidthUint32,
+    ExtendCap,
+    1,
+    &UsbLegSup
+    );
+
+  IsOwnershipConflict = IsBiosOwned && IsOsOwned;
+
+  if (IsOwnershipConflict) {
+    //
+    // EHCI - Ownership conflict - attempting soft reset.
+    //
+    Value = 0;
+    PciIo->Pci.Write (
+      PciIo,
+      EfiPciIoWidthUint8,
+      ExtendCap + 3,
+      1,
+      &Value
+      );
+
+    TimeOut = 40;
+    while (TimeOut--) {
+      gBS->Stall (500);
+
+      PciIo->Pci.Read (
+        PciIo,
+        EfiPciIoWidthUint32,
+        ExtendCap,
+        1,
+        &Value
+        );
+
+      if ((Value & BIT24) == 0x0) {
+        break;
+      }
+    }
+  }
+
+  PciIo->Pci.Read (
+    PciIo,
+    EfiPciIoWidthUint32,
+    ExtendCap,
+    1,
+    &Value
+    );
+
+  Value |= BIT24;
+  PciIo->Pci.Write (
+    PciIo,
+    EfiPciIoWidthUint32,
+    ExtendCap,
+    1,
+    &Value
+    );
+
+  TimeOut = 40;
+  while (TimeOut--) {
+    gBS->Stall (500);
+
+    PciIo->Pci.Read (
+      PciIo,
+      EfiPciIoWidthUint32,
+      ExtendCap,
+      1,
+      &Value
+      );
+
+    if ((Value & BIT16) == 0x0) {
+      break;
+    }
+  }
+
+  IsOwnershipConflict = (Value & BIT16) != 0x0;
+  if (IsOwnershipConflict) {
+    //
+    // Soft reset has failed. Assume SMI being ignored and do hard reset.
+    //
+    Value = 0;
+    PciIo->Pci.Write (
+      PciIo,
+      EfiPciIoWidthUint8,
+      ExtendCap + 2,
+      1,
+      &Value
+      );
+
+    TimeOut = 40;
+    while (TimeOut--) {
+      gBS->Stall (500);
+
+      PciIo->Pci.Read (
+        PciIo,
+        EfiPciIoWidthUint32,
+        ExtendCap,
+        1,
+        &Value
+        );
+
+      if ((Value & BIT16) == 0x0) {
+        break;
+      }
+    }
+
+    //
+    // Disable further SMI events.
+    //
+    PciIo->Pci.Read (
+      PciIo,
+      EfiPciIoWidthUint32,
+      ExtendCap + 0x4,
+      1,
+      &UsbLegCtlSts
+      );
+
+    UsbLegCtlSts &= 0xFFFF0000U;
+    PciIo->Pci.Write (
+      PciIo,
+      EfiPciIoWidthUint32,
+      ExtendCap + 0x4,
+      1,
+      &UsbLegCtlSts
+      );
+  }
+
+  if (Value & BIT16) {
+    //
+    // EHCI controller unable to take control from BIOS.
+    //
+    Status = EFI_NOT_FOUND;
+  }
+
+  return Status;
+}
+
+/**
+ Release UHCI USB controllers ownership.
+
+ @param[in]  PciIo  PCI I/O protocol for the device.
+
+ @retval EFI_NOT_FOUND  No UHCI controllers had ownership incorrectly set.
+ @retval EFI_SUCCESS    Corrected UHCI controllers ownership to OS.
+ **/
+STATIC
+EFI_STATUS
+UhciReleaseOwnership (
+  IN  EFI_PCI_IO_PROTOCOL   *PciIo
+  )
+{
+  EFI_STATUS  Status;
+  UINT32      Base;
+  UINT32      PortBase;
+  UINT16      Command;
+
+  Base = 0;
+
+  Status = PciIo->Pci.Read(
+    PciIo,
+    EfiPciIoWidthUint32,
+    0x20,
+    1,
+    &Base
+    );
+
+  PortBase = (Base >> 5) & 0x07ff;
+
+  Command = 0x8f00;
+
+  Status = PciIo->Pci.Write (
+    PciIo,
+    EfiPciIoWidthUint16,
+    0xC0,
+    1,
+    &Command
+    );
+
+  if (PortBase) {
+    IoWrite16 (PortBase, 0x0002);
+    gBS->Stall (500);
+    IoWrite16 (PortBase + 4, 0);
+    gBS->Stall (500);
+    IoWrite16 (PortBase, 0);
+  }
+
+  return Status;
+}
+
 EFI_STATUS
 ReleaseUsbOwnership (
   VOID
@@ -235,11 +607,12 @@ ReleaseUsbOwnership (
       continue;
     }
 
-    //
-    // FIXME: Check whether we need PCI_IF_UHCI and PCI_IF_EHCI as well.
-    //
     if (Pci.Hdr.ClassCode[0] == PCI_IF_XHCI) {
       Result = XhciReleaseOwnership (PciIo);
+    } else if (Pci.Hdr.ClassCode[0] == PCI_IF_EHCI) {
+      Result = EhciReleaseOwnership (PciIo);
+    } else if (Pci.Hdr.ClassCode[0] == PCI_IF_UHCI) {
+      Result = UhciReleaseOwnership (PciIo);
     }
   }
 
