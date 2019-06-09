cafd83b25b775e709ec29cc6c824942198369578
vit9696 <vit9696@users.noreply.github.com>
2019-06-09 19:37:43 +0300

    OcConfigurationLib: Add Enabled property to Tools

--- Include/Library/OcConfigurationLib.h
+++ Include/Library/OcConfigurationLib.h
@@ -245,6 +245,7 @@
 
 #define OC_MISC_TOOLS_ENTRY_FIELDS(_, __) \
   _(OC_STRING                   , Comment          ,     , OC_STRING_CONSTR ("", _, __), OC_DESTR (OC_STRING) ) \
+  _(BOOLEAN                     , Enabled          ,     , FALSE                       , ()                   ) \
   _(OC_STRING                   , Name             ,     , OC_STRING_CONSTR ("", _, __), OC_DESTR (OC_STRING) ) \
   _(OC_STRING                   , Path             ,     , OC_STRING_CONSTR ("", _, __), OC_DESTR (OC_STRING) )
   OC_DECLARE (OC_MISC_TOOLS_ENTRY)
