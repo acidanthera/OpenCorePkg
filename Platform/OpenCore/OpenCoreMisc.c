461eb44fad12df2d7e4ef6722134c77fa9b2c12b
vit9696 <vit9696@users.noreply.github.com>
2019-06-09 19:40:29 +0300

    OpenCoreMisc: Add Enabled property to Tools

--- Platform/OpenCore/OpenCoreMisc.c
+++ Platform/OpenCore/OpenCoreMisc.c
@@ -322,6 +322,7 @@ OcMiscBoot (
   OC_PICKER_CONTEXT  *Context;
   UINTN              ContextSize;
   UINT32             Index;
+  UINT32             ToolCount;
 
   //
   // Do not use our boot picker unless asked.
@@ -354,14 +355,20 @@ OcMiscBoot (
   Context->ShowPicker         = Config->Misc.Boot.ShowPicker;
   Context->CustomBootGuid     = CustomBootGuid;
   Context->ExcludeHandle      = LoadHandle;
-  Context->CustomEntryCount   = Config->Misc.Tools.Count;
   Context->CustomEntryContext = Storage;
   Context->CustomRead         = OcToolLoadEntry;
 
+  ToolCount = 0;
+
   for (Index = 0; Index < Context->CustomEntryCount; ++Index) {
-    Context->CustomEntries[Index].Name = OC_BLOB_GET (&Config->Misc.Tools.Values[Index]->Name);
-    Context->CustomEntries[Index].Path = OC_BLOB_GET (&Config->Misc.Tools.Values[Index]->Path);
+    if (Config->Misc.Tools.Values[Index]->Enabled) {
+      Context->CustomEntries[Index].Name = OC_BLOB_GET (&Config->Misc.Tools.Values[Index]->Name);
+      Context->CustomEntries[Index].Path = OC_BLOB_GET (&Config->Misc.Tools.Values[Index]->Path);
+      ++ToolCount;
+    }
   }
+
+  Context->CustomEntryCount = ToolCount;
 
   Status = OcRunSimpleBootPicker (
     Context
