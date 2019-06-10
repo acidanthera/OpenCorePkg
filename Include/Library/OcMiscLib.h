2825930a0de5550dd27409e659ad343511a109a0
vit9696 <vit9696@users.noreply.github.com>
2019-06-10 19:59:23 +0300

    OcMiscLib: Force our own base64 implementation, edk2 is broken
    Revert "Deprecate OcBase64Decode in favor of edk2's Base64Decode"
    This reverts commit 2104029260a009b807614b10aaee931685840282.
    Reference: https://github.com/acidanthera/bugtracker/issues/372

--- Include/Library/OcMiscLib.h
+++ Include/Library/OcMiscLib.h
@@ -23,6 +23,27 @@
 **/
 #define SECONDS_TO_MICROSECONDS(x) ((x)*1000000)
 
+/**
+  TODO: EDK II has its implementation in BaseLib, but it is broken,
+  as it fails to update DecodedLength:
+  https://github.com/acidanthera/bugtracker/issues/372
+
+  @param[in] EncodedData        A pointer to the data to convert.
+  @param[in] EncodedLength      The length of data to convert.
+  @param[in] DecodedData        A pointer to location to store the decoded data.
+  @param[in] DecodedSize        A pointer to location to store the decoded size.
+
+  @retval  TRUE on success.
+**/
+RETURN_STATUS
+EFIAPI
+OcBase64Decode (
+  IN     CONST CHAR8  *EncodedData,
+  IN     UINTN        EncodedLength,
+     OUT UINT8        *DecodedData,
+  IN OUT UINTN        *DecodedLength
+  );
+
 // LegacyRegionlock
 /** Lock the legacy region specified to enable modification.
 
