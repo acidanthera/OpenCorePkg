9c0da8f57ffe4bbc17e59bc55420f0e08168c07e
vit9696 <vit9696@users.noreply.github.com>
2019-07-26 19:43:29 +0300

    OcMemoryLib: Add virtual memory support

--- Include/Library/OcMemoryLib.h
+++ Include/Library/OcMemoryLib.h
@@ -15,6 +15,8 @@
 #ifndef OC_MEMORY_LIB_H
 #define OC_MEMORY_LIB_H
 
+#include <IndustryStandard/VirtualMemory.h>
+
 /**
   Lock the legacy region specified to enable modification.
 
@@ -86,7 +88,7 @@ GetCurrentMemoryMapAlloc (
   );
 
 /**
-  Shrinks memory map by joining non-runtime records.
+  Shrink memory map by joining non-runtime records.
 
   @param[in,out]  MemoryMapSize      Memory map size in bytes, updated on shrink.
   @param[in,out]  MemoryMap          Memory map to shrink.
@@ -115,7 +117,7 @@ BOOLEAN
   );
 
 /**
-  Alloctes pages from the top of physical memory up to address specified in Memory.
+  Allocate pages from the top of physical memory up to address specified in Memory.
   Unlike AllocateMaxAddress, this method guarantees to choose top most address.
 
   @param[in]      MemoryType       Allocated memory type.
@@ -126,13 +128,138 @@ BOOLEAN
 
   @retval EFI_SUCCESS on successful allocation.
 **/
-RETURN_STATUS
+EFI_STATUS
 AllocatePagesFromTop (
   IN     EFI_MEMORY_TYPE         MemoryType,
   IN     UINTN                   Pages,
   IN OUT EFI_PHYSICAL_ADDRESS    *Memory,
   IN     EFI_GET_MEMORY_MAP      GetMemoryMap  OPTIONAL,
   IN     CHECK_ALLOCATION_RANGE  CheckRange  OPTIONAL
+  );
+
+/**
+  Return pointer to PML4 table in PageTable and PWT and PCD flags in Flags.
+
+  @param[out]  PageTable  Current page table address.
+  @param[out]  Flags      Current page table PWT and PCT flags.
+**/
+VOID
+GetCurrentPageTable (
+  OUT PAGE_MAP_AND_DIRECTORY_POINTER  **PageTable,
+  OUT UINTN                           *Flags  OPTIONAL
+  );
+
+/**
+  Return physical addrress for given virtual addrress.
+
+  @param[in]  PageTable       Page table to use for solving.
+  @param[in]  VirtualAddr     Virtual address to look up.
+  @param[out] PhysicalAddr    Physical address to return.
+
+  @retval EFI_SUCCESS on successful lookup.
+**/
+EFI_STATUS
+GetPhysicalAddress (
+  IN  PAGE_MAP_AND_DIRECTORY_POINTER   *PageTable  OPTIONAL,
+  IN  EFI_VIRTUAL_ADDRESS              VirtualAddr,
+  OUT EFI_PHYSICAL_ADDRESS             *PhysicalAddr
+  );
+
+/**
+  Virtual memory context
+**/
+typedef struct OC_VMEM_CONTEXT_ {
+  ///
+  /// Memory pool containing memory to be spread across allocations.
+  ///
+  UINT8  *MemoryPool;
+  ///
+  /// Free pages in the memory pool.
+  ///
+  UINTN  FreePages;
+} OC_VMEM_CONTEXT;
+
+/**
+  Reasonable default virtual memory page pool size (2 MB).
+**/
+#define OC_DEFAULT_VMEM_PAGE_COUNT 0x200
+
+/**
+  Allocate EfiBootServicesData virtual memory pool from boot services
+  in the end of BASE_4GB of RAM. Should be called while boot services are
+  still usable.
+
+  @param[out]  Context   Virtual memory pool context.
+  @param[in]   NumPages  Number of pages to be allocated in the pool.
+
+  @retval EFI_SUCCESS on successful allocation.
+**/
+EFI_STATUS
+VmAllocateMemoryPool (
+  OUT OC_VMEM_CONTEXT  *Context,
+  IN  UINTN            NumPages
+  );
+
+/**
+  Allocate pages for e.g. vm page maps.
+
+  @param[in,out]  Context   Virtual memory pool context.
+  @param[in]      NumPages  Number of pages to allocate.
+
+  @retval allocated pages or NULL.
+**/
+VOID *
+VmAllocatePages (
+  IN OUT OC_VMEM_CONTEXT  *Context,
+  IN     UINTN            NumPages
+  );
+
+/**
+  Map (remap) given page at physical address to given virtual address in
+  the specified page table.
+
+  @param[in,out]  Context       Virtual memory pool context.
+  @param[in]      PageTable     Page table to update.
+  @param[in]      VirtualAddr   Virtual memory address to map at.
+  @param[in]      PhysicalAddr  Physical memory address to map from.
+
+  @retval EFI_SUCCESS on success.
+**/
+EFI_STATUS
+VmMapVirtualPage (
+  IN OUT OC_VMEM_CONTEXT                 *Context,
+  IN OUT PAGE_MAP_AND_DIRECTORY_POINTER  *PageTable  OPTIONAL,
+  IN     EFI_VIRTUAL_ADDRESS             VirtualAddr,
+  IN     EFI_PHYSICAL_ADDRESS            PhysicalAddr
+  );
+
+/**
+  Map (remap) a range of 4K pages at physical address to given virtual address
+  in the specified page table.
+
+  @param[in,out]  Context       Virtual memory pool context.
+  @param[in]      PageTable     Page table to update.
+  @param[in]      VirtualAddr   Virtual memory address to map at.
+  @param[in]      NumPages      Number of 4K pages to map.
+  @param[in]      PhysicalAddr  Physical memory address to map from.
+
+  @retval EFI_SUCCESS on success.
+**/
+EFI_STATUS
+VmMapVirtualPages (
+  IN OUT OC_VMEM_CONTEXT                 *Context,
+  IN OUT PAGE_MAP_AND_DIRECTORY_POINTER  *PageTable  OPTIONAL,
+  IN     EFI_VIRTUAL_ADDRESS             VirtualAddr,
+  IN     UINTN                           NumPages,
+  IN     EFI_PHYSICAL_ADDRESS            PhysicalAddr
+  );
+
+/**
+  Flushes TLB caches.
+**/
+VOID
+VmFlushCaches (
+  VOID
   );
 
 #endif // OC_MEMORY_LIB_H
