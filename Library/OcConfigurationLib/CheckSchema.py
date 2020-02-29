5e36af1bca16adfcd770d3ac468749e42972d15a
vit9696 <vit9696@users.noreply.github.com>
2020-02-29 22:19:57 +0300

    OcConfigurationLib: Add python 2 compatibility in CheckSchema.py

--- Library/OcConfigurationLib/CheckSchema.py
+++ Library/OcConfigurationLib/CheckSchema.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 """
 Validates schema files (e.g. OcConfigurationLib.c) for being sorted.
@@ -16,12 +16,12 @@ with open(sys.argv[1], 'r') as f:
   content = [l.strip() for l in f.readlines()]
   for i, l in enumerate(content):
     if l == 'OC_SCHEMA':
-      print('Checking schema {}'.format(re.match('^\w+', content[i+1])[0]))
+      print('Checking schema {}'.format(re.match('^\w+', content[i+1]).group(0)))
       prev = ''
       continue
     x = re.search('"([^"]+)"', l)
     if x:
-      if x[1] < prev:
-        print('ERROR: {} succeeds {}'.format(prev, x[1]))
+      if x.group(1) < prev:
+        print('ERROR: {} succeeds {}'.format(prev, x.group(1)))
         sys.exit(1)
-      prev = x[1]
+      prev = x.group(1)
