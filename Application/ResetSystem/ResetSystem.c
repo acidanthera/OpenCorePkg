3e8d35f6f5568ef3a2a1e5cfbed18f4b5dbb673b
Zhen <66577170+zhen-zen@users.noreply.github.com>
2020-06-14 11:25:29 -0700

    OCRST: Support reboot into firmware

--- Application/ResetSystem/ResetSystem.c
+++ Application/ResetSystem/ResetSystem.c
@@ -12,6 +12,7 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 **/
 
+#include <Guid/GlobalVariable.h>
 #include <Uefi.h>
 #include <Library/BaseMemoryLib.h>
 #include <Library/OcDebugLogLib.h>
@@ -33,6 +34,9 @@ UefiMain (
   CHAR16            **Argv;
   CHAR16            *Mode;
   EFI_RESET_TYPE    ResetMode;
+  UINT64            OsIndications;
+  UINT32            Attr;
+  UINTN             DataSize;
 
   Status = GetArguments (&Argc, &Argv);
   if (!EFI_ERROR (Status) && Argc >= 2) {
@@ -40,6 +44,38 @@ UefiMain (
   } else {
     DEBUG ((DEBUG_INFO, "OCRST: Assuming default to be ResetCold - %r\n", Status));
     Mode = L"ColdReset";
+  }
+
+  if (StrCmp (Mode, L"Firmware") == 0) {
+    DEBUG ((DEBUG_INFO, "OCRST: Entering firmware...\n"));
+    DataSize = sizeof (OsIndications);
+    Status = gRT->GetVariable (
+      EFI_OS_INDICATIONS_SUPPORT_VARIABLE_NAME, &gEfiGlobalVariableGuid,
+      &Attr, &DataSize, &OsIndications
+      );
+    if (!EFI_ERROR (Status)) {
+      if ((OsIndications & EFI_OS_INDICATIONS_BOOT_TO_FW_UI) != 0) {
+        DataSize = sizeof (OsIndications);
+        Status = gRT->GetVariable (
+          EFI_OS_INDICATIONS_VARIABLE_NAME, &gEfiGlobalVariableGuid,
+          &Attr, &DataSize, &OsIndications
+          );
+        if (!EFI_ERROR (Status)) {
+          OsIndications |= EFI_OS_INDICATIONS_BOOT_TO_FW_UI;
+        } else {
+          OsIndications = EFI_OS_INDICATIONS_BOOT_TO_FW_UI;
+        }
+        Status = gRT->SetVariable (
+          EFI_OS_INDICATIONS_VARIABLE_NAME, &gEfiGlobalVariableGuid,
+          EFI_VARIABLE_NON_VOLATILE | EFI_VARIABLE_BOOTSERVICE_ACCESS | EFI_VARIABLE_RUNTIME_ACCESS,
+          sizeof (OsIndications), &OsIndications
+          );
+      }
+    }
+    if (EFI_ERROR (Status)) {
+      DEBUG ((DEBUG_INFO, "OCRST: Failed to enter firmware...\n"));
+      return EFI_SUCCESS;
+    }
   }
 
   if (StrCmp (Mode, L"ColdReset") == 0) {
