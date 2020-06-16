d6c5c5c762c1922cee950171a1d896f4b78185d7
zhen-zen <66577170+zhen-zen@users.noreply.github.com>
2020-06-16 10:49:43 -0700

    ResetSystem: Support reboot into firmware (#73)

--- Application/ResetSystem/ResetSystem.c
+++ Application/ResetSystem/ResetSystem.c
@@ -12,6 +12,7 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 **/
 
+#include <Guid/GlobalVariable.h>
 #include <Uefi.h>
 #include <Library/BaseMemoryLib.h>
 #include <Library/OcDebugLogLib.h>
@@ -33,12 +34,62 @@ UefiMain (
   CHAR16            **Argv;
   CHAR16            *Mode;
   EFI_RESET_TYPE    ResetMode;
+  UINT64            OsIndications;
+  UINT32            Attr;
+  UINTN             DataSize;
 
   Status = GetArguments (&Argc, &Argv);
   if (!EFI_ERROR (Status) && Argc >= 2) {
     Mode = Argv[1];
   } else {
     DEBUG ((DEBUG_INFO, "OCRST: Assuming default to be ResetCold - %r\n", Status));
+    Mode = L"ColdReset";
+  }
+
+  if (StrCmp (Mode, L"Firmware") == 0) {
+    DEBUG ((DEBUG_INFO, "OCRST: Entering firmware...\n"));
+    DataSize = sizeof (OsIndications);
+    Status = gRT->GetVariable (
+                    EFI_OS_INDICATIONS_SUPPORT_VARIABLE_NAME,
+                    &gEfiGlobalVariableGuid,
+                    &Attr,
+                    &DataSize,
+                    &OsIndications
+                    );
+    if (!EFI_ERROR (Status)) {
+      if ((OsIndications & EFI_OS_INDICATIONS_BOOT_TO_FW_UI) != 0) {
+        DataSize = sizeof (OsIndications);
+        Status = gRT->GetVariable (
+                        EFI_OS_INDICATIONS_VARIABLE_NAME,
+                        &gEfiGlobalVariableGuid,
+                        &Attr,
+                        &DataSize,
+                        &OsIndications
+                        );
+        if (!EFI_ERROR (Status)) {
+          OsIndications |= EFI_OS_INDICATIONS_BOOT_TO_FW_UI;
+        } else {
+          OsIndications = EFI_OS_INDICATIONS_BOOT_TO_FW_UI;
+        }
+        Status = gRT->SetVariable (
+                        EFI_OS_INDICATIONS_VARIABLE_NAME,
+                        &gEfiGlobalVariableGuid,
+                        EFI_VARIABLE_NON_VOLATILE | EFI_VARIABLE_BOOTSERVICE_ACCESS | EFI_VARIABLE_RUNTIME_ACCESS,
+                        sizeof (OsIndications),
+                        &OsIndications
+                        );
+        if (EFI_ERROR (Status)) {
+          DEBUG ((DEBUG_WARN, "OCRST: Failed to set EFI_OS_INDICATIONS_BOOT_TO_FW_UI - %r\n", Status));
+          return EFI_ABORTED;
+        }
+      } else {
+        DEBUG ((DEBUG_WARN, "OCRST: Firmware do not support EFI_OS_INDICATIONS_BOOT_TO_FW_UI - %r\n", Status));
+        return EFI_UNSUPPORTED;
+      }
+    } else {
+      DEBUG ((DEBUG_WARN, "OCRST: Failed to acquire firmware features - %r\n", Status));
+      return EFI_NOT_FOUND;
+    }
     Mode = L"ColdReset";
   }
 
