a759d004ace2eb704cf3a17b9b7c1353da9cc84b
vit9696 <vit9696@users.noreply.github.com>
2020-08-08 08:24:16 +0300

    Fix compilation

---
+++ User/Library/OcDummy.c
@@ -0,0 +1,15 @@
+/** @file
+  Copyright (c) 2020, PMheart. All rights reserved.
+  SPDX-License-Identifier: BSD-3-Clause
+**/
+
+#include <Library/OcDevicePathLib.h>
+
+EFI_DEVICE_PATH_PROTOCOL *
+OcGetNextLoadOptionDevicePath (
+  IN  EFI_DEVICE_PATH_PROTOCOL          *FilePath,
+  IN  EFI_DEVICE_PATH_PROTOCOL          *FullPath
+  )
+{
+  return NULL;
+}
