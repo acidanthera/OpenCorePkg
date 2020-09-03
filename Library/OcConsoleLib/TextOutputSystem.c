bd444369ac3a8186e7b429e00e614210409eece4
dakanji <dakanji@users.noreply.github.com>
2020-09-03 16:02:36 +0300

    Return Status on Setting Renderers

--- Library/OcConsoleLib/TextOutputSystem.c
+++ Library/OcConsoleLib/TextOutputSystem.c
@@ -292,7 +292,7 @@ ConsoleControlSetMode (
   //
   if (Mode == EfiConsoleControlScreenGraphics) {
     gST->ConOut->EnableCursor (gST->ConOut, FALSE);
-    gST->ConOut->SetCursorPosition (gST->ConOut, 0, 0);  
+    gST->ConOut->SetCursorPosition (gST->ConOut, 0, 0);
   }
 
   return EFI_SUCCESS;
@@ -324,7 +324,7 @@ mConsoleControlProtocol = {
   ConsoleControlLockStdIn
 };
 
-VOID
+EFI_STATUS
 OcUseSystemTextOutput (
   IN OC_CONSOLE_RENDERER          Renderer,
   IN BOOLEAN                      IgnoreTextOutput,
@@ -365,4 +365,6 @@ OcUseSystemTextOutput (
     mOriginalClearScreen      = gST->ConOut->ClearScreen;
     gST->ConOut->ClearScreen  = ControlledClearScreen;
   }
+
+  return EFI_SUCCESS;
 }
