0459b9752d10c9e4e74f0d51c8435530a85d609c
Andrey1970AppleLife <ua6lpo@rambler.ru>
2020-11-02 23:51:07 +0300

    Bump version to 0.6.4

--- Include/Acidanthera/OpenCore.h
+++ Include/Acidanthera/OpenCore.h
@@ -31,7 +31,7 @@
   OpenCore version reported to log and NVRAM.
   OPEN_CORE_VERSION must follow X.Y.Z format, where X.Y.Z are single digits.
 **/
-#define OPEN_CORE_VERSION          "0.6.3"
+#define OPEN_CORE_VERSION          "0.6.4"
 
 /**
   OpenCore build type reported to log and NVRAM.
