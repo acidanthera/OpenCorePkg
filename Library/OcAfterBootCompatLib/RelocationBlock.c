3a86b9e78b0f23353a4cf12570ba09022d9cea4a
vit9696 <vit9696@users.noreply.github.com>
2020-11-10 23:29:43 +0300

    Revert "OcAfterBootCompatLib: Add 11.0 detection (freezes on ExitBS)"
    This reverts commit 69f8881418eee9dab7f54faec4fc926aa6d70e6d.

--- Library/OcAfterBootCompatLib/RelocationBlock.c
+++ Library/OcAfterBootCompatLib/RelocationBlock.c
@@ -53,11 +53,7 @@ AppleRelocationAllocatePages (
     return EFI_UNSUPPORTED;
   }
 
-  //
-  // Operating systems up to macOS 10.15 allocate starting with TEXT segment (2MB).
-  // macOS 11.0 allocates starting with HIB segment (1MB).
-  //
-  if (*Memory >= KERNEL_BASE_PADDR && *Memory <= KERNEL_TEXT_PADDR && BootCompat->KernelState.RelocationBlock == 0) {
+  if (*Memory == KERNEL_TEXT_PADDR && BootCompat->KernelState.RelocationBlock == 0) {
     BootCompat->KernelState.RelocationBlock = BASE_4GB;
     Status = OcAllocatePagesFromTop (
       EfiLoaderData,
