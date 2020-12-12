0c7aaeaad15301262fc58dbaccd05950e9c76fe6
PMheart <17109513+PMheart@users.noreply.github.com>
2020-12-12 19:11:39 +0800

    Utilities: Adapt to EDK II code style for ocvalidate (WIP)

--- Utilities/ocvalidate/ocvalidate.c
+++ Utilities/ocvalidate/ocvalidate.c
@@ -37,248 +37,384 @@
  rm -rf ConfigValidty.dSYM DICT fuzz*.log ConfigValidty
 */
 
+//
+// TODO: Doc functions and add comments where necessary.
+//
 
-long long current_timestamp() {
-    struct timeval te;
-    gettimeofday(&te, NULL); // get current time
-    long long milliseconds = te.tv_sec*1000LL + te.tv_usec/1000; // calculate milliseconds
-    // printf("milliseconds: %lld\n", milliseconds);
-    return milliseconds;
+typedef
+UINT32
+(*CONFIG_CHECK) (
+  IN  OC_GLOBAL_CONFIG  *Config
+  );
+
+STATIC
+INT64
+GetCurrentTimestamp (
+  VOID
+  )
+{
+  struct timeval te;
+
+  //
+  // Get current time.
+  //
+  gettimeofday (&te, NULL);
+  //
+  // Return milliseconds.
+  //
+  return te.tv_sec * 1000LL + te.tv_usec / 1000LL;
 }
 
-const char *get_filename_suffix(const char *filename) {
-  // find the last dot by reverse order
-  const char *suffix_dot = strrchr(filename, '.');
+STATIC
+CHAR8 *
+GetFilenameSuffix (
+  IN  CONST CHAR8  *FileName
+  )
+{
+  //
+  // Find the last dot by reverse order.
+  //
+  CONST CHAR8  *SuffixDot;
 
-  // in some weird cases the filename can be crazily like
+  SuffixDot = strrchr (FileName, '.');
+
+  //
+  // In some weird cases the filename can be crazily like
   // "a.suffix." (note that '.') or "..." (extremely abnormal).
-  // we do not support such.
-  if (!suffix_dot || suffix_dot == filename)
-    return "";
+  // We do not support such.
+  //
+  if (!SuffixDot || SuffixDot == FileName) {
+    return (CHAR8 *) "";
+  }
 
-  // in most normal cases, return whatever following the dot.
-  return suffix_dot + 1;
+  //
+  // In most normal cases, return whatever following the dot.
+  //
+  return (CHAR8 *) SuffixDot + 1;
 }
 
-bool str_has_all_legal_char(const char *str) {
+STATIC
+BOOLEAN
+StringHasAllLegalCharacter (
+  IN  CONST CHAR8  *String
+  )
+{
   // only 0-9, A-Z, a-z, '_', '-', '.', '/', and '\'
   // are accepted.
+  UINTN  Index;
 
-  for (size_t i = 0; i < strlen(str); i++) {
-    if (isdigit(str[i])) continue;
-    if (isalpha(str[i])) continue;
-    if (str[i] == '_')   continue;
-    if (str[i] == '-')   continue;
-    if (str[i] == '.')   continue;
-    if (str[i] == '/')   continue;
-    if (str[i] == '\\')  continue;
+  for (Index = 0; Index < AsciiStrLen (String); ++Index) {
+    //
+    // Skip allowed characters.
+    //
+    if (isdigit (String[Index])
+      || isalpha (String[Index])
+      || String[Index] == '_'
+      || String[Index] == '-'
+      || String[Index] == '.'
+      || String[Index] == '/'
+      || String[Index] == '\\') {
+      continue;
+    }
 
-    // return false when matching characters
-    // other than those ones,
-    // which are not accepted due to sanitising.
-    return false;
+    //
+    // Disallowed characters matched.
+    //
+    return FALSE;
   }
 
-  return true;
+  return TRUE;
 }
 
-bool str_has_all_printable_char(const char *str) {
-  for (size_t i = 0; i < strlen(str); i++) {
-    if (isprint(str[i]) == 0) return false;
+STATIC
+BOOLEAN
+StringHasAllPrintableCharacter (
+  IN  CONST CHAR8  *String
+  )
+{
+  UINTN  Index;
+
+  for (Index = 0; Index < AsciiStrLen (String); ++Index) {
+    //
+    // Unprintable characters matched.
+    //
+    if (isprint (String[Index]) == 0) {
+      return FALSE;
+    }
   }
 
-  return true;
+  return TRUE;
 }
 
-unsigned int check_ACPI(OC_GLOBAL_CONFIG *Config) {
-  unsigned int    ret = 0;
+STATIC
+UINT32
+CheckACPI (
+  IN  OC_GLOBAL_CONFIG  *Config
+  )
+{
+  UINT32          ErrorCount;
+  UINT32          Index;
   OC_ACPI_CONFIG  UserAcpi;
-  const char      *path;
-  const char      *comment;
-  bool            has_custom_dsdt = false;
-  // CHAR16          *PathUnicode;
+  CONST CHAR8     *Path;
+  CONST CHAR8     *Comment;
+  BOOLEAN         HasCustomDSDT;
 
   DEBUG ((DEBUG_INFO, "config loaded into ACPI checker!\n"));
 
-  UserAcpi = Config->Acpi;
+  UserAcpi      = Config->Acpi;
+  ErrorCount    = 0;
+  HasCustomDSDT = FALSE;
 
-  for (uint32_t i = 0; i < UserAcpi.Add.Count; i++) {
-    path    = OC_BLOB_GET (&UserAcpi.Add.Values[i]->Path);
-    comment = OC_BLOB_GET (&UserAcpi.Add.Values[i]->Comment);
+  for (Index = 0; Index < UserAcpi.Add.Count; ++Index) {
+    Path    = OC_BLOB_GET (&UserAcpi.Add.Values[Index]->Path);
+    Comment = OC_BLOB_GET (&UserAcpi.Add.Values[Index]->Comment);
 
-    if (!str_has_all_legal_char(path)) {
-      DEBUG ((DEBUG_WARN, "ACPI->Add[%u]->Path contains illegal character!\n", i));
-      ret++;
+    if (!StringHasAllLegalCharacter (Path)) {
+      DEBUG ((DEBUG_WARN, "ACPI->Add[%u]->Path contains illegal character!\n", Index));
+      ++ErrorCount;
     }
 
-    if (!str_has_all_printable_char(comment)) {
-      DEBUG ((DEBUG_WARN, "ACPI->Add[%u]->Comment contains illegal character!\n", i));
-      ret++;
+    if (!StringHasAllPrintableCharacter (Comment)) {
+      DEBUG ((DEBUG_WARN, "ACPI->Add[%u]->Comment contains illegal character!\n", Index));
+      ++ErrorCount;
     }
 
-    if (strcmp(get_filename_suffix(path), "dsl") == 0) {
-      DEBUG ((DEBUG_WARN, "ACPI->Add[%u]->Path has .dsl suffix!\n", i));
-      ret++;
+    if (AsciiStrCmp (GetFilenameSuffix (Path), "dsl") == 0) {
+      DEBUG ((DEBUG_WARN, "ACPI->Add[%u]->Path has .dsl suffix!\n", Index));
+      ++ErrorCount;
     }
 
-    if (OcAsciiStriStr (path, "DSDT") == 0) {
-      has_custom_dsdt = true;
+    if (OcAsciiStriStr (Path, "DSDT") == 0) {
+      HasCustomDSDT = TRUE;
     }
   }
 
-  // for (uint32_t i = 0; i < UserAcpi.Delete.Count; i++) {
+  for (Index = 0; Index < UserAcpi.Delete.Count; Index++) {
+    //
+    // TODO
+    //
+  }
 
-  // }
-
-  if (has_custom_dsdt && !UserAcpi.Quirks.RebaseRegions) {
+  if (HasCustomDSDT && !UserAcpi.Quirks.RebaseRegions) {
     DEBUG ((DEBUG_WARN, "ACPI->Quirks->RebaseRegions is not enabled when there is a custom DSDT table!\n"));
-    ret++;
+    ++ErrorCount;
   }
 
-  if (ret != 0)
-    DEBUG ((DEBUG_WARN, "%a returns %u %a!\n", __func__, ret, ret > 1 ? "errors" : "error"));
-  return ret;
+  if (ErrorCount != 0) {
+    DEBUG ((DEBUG_WARN, "%a returns %u %a!\n", __func__, ErrorCount, ErrorCount > 1 ? "errors" : "error"));
+  }
+  return ErrorCount;
 }
 
-unsigned int check_Booter(OC_GLOBAL_CONFIG *Config) {
-  unsigned int ret = 0;
+STATIC
+UINT32
+CheckBooter (
+  IN  OC_GLOBAL_CONFIG  *Config
+  )
+{
+  UINT32 ErrorCount;
 
   DEBUG ((DEBUG_INFO, "config loaded into Booter checker!\n"));
 
-  if (ret != 0)
-    DEBUG ((DEBUG_WARN, "%a returns %u %a!\n", __func__, ret, ret > 1 ? "errors" : "error"));
-  return ret;
+  ErrorCount = 0;
+
+  if (ErrorCount != 0) {
+    DEBUG ((DEBUG_WARN, "%a returns %u %a!\n", __func__, ErrorCount, ErrorCount > 1 ? "errors" : "error"));
+  }
+  return ErrorCount;
 }
 
-unsigned int check_DeviceProperties(OC_GLOBAL_CONFIG *Config) {
-  unsigned int ret = 0;
+STATIC
+UINT32
+CheckDeviceProperties (
+  IN  OC_GLOBAL_CONFIG  *Config
+  )
+{
+  UINT32 ErrorCount;
 
   DEBUG ((DEBUG_INFO, "config loaded into DeviceProperties checker!\n"));
 
-  if (ret != 0)
-    DEBUG ((DEBUG_WARN, "%a returns %u %a!\n", __func__, ret, ret > 1 ? "errors" : "error"));
-  return ret;
+  ErrorCount = 0;
+
+  if (ErrorCount != 0) {
+    DEBUG ((DEBUG_WARN, "%a returns %u %a!\n", __func__, ErrorCount, ErrorCount > 1 ? "errors" : "error"));
+  }
+  return ErrorCount;
 }
 
-unsigned int check_Kernel(OC_GLOBAL_CONFIG *Config) {
-  unsigned int ret = 0;
+STATIC
+UINT32
+CheckKernel (
+  IN  OC_GLOBAL_CONFIG  *Config
+  )
+{
+  UINT32 ErrorCount;
 
   DEBUG ((DEBUG_INFO, "config loaded into Kernel checker!\n"));
 
-  if (ret != 0)
-    DEBUG ((DEBUG_WARN, "%a returns %u %a!\n", __func__, ret, ret > 1 ? "errors" : "error"));
-  return ret;
+  ErrorCount = 0;
+
+  if (ErrorCount != 0) {
+    DEBUG ((DEBUG_WARN, "%a returns %u %a!\n", __func__, ErrorCount, ErrorCount > 1 ? "errors" : "error"));
+  }
+  return ErrorCount;
 }
 
-unsigned int check_Misc(OC_GLOBAL_CONFIG *Config) {
-  unsigned int ret = 0;
+STATIC
+UINT32
+CheckMisc (
+  IN  OC_GLOBAL_CONFIG  *Config
+  )
+{
+  UINT32 ErrorCount;
 
   DEBUG ((DEBUG_INFO, "config loaded into Misc checker!\n"));
 
-  if (ret != 0)
-    DEBUG ((DEBUG_WARN, "%a returns %u %a!\n", __func__, ret, ret > 1 ? "errors" : "error"));
-  return ret;
+  ErrorCount = 0;
+
+  if (ErrorCount != 0) {
+    DEBUG ((DEBUG_WARN, "%a returns %u %a!\n", __func__, ErrorCount, ErrorCount > 1 ? "errors" : "error"));
+  }
+  return ErrorCount;
 }
 
-unsigned int check_NVRAM(OC_GLOBAL_CONFIG *Config) {
-  unsigned int ret = 0;
+STATIC
+UINT32
+CheckNVRAM (
+  IN  OC_GLOBAL_CONFIG  *Config
+  )
+{
+  UINT32 ErrorCount;
 
   DEBUG ((DEBUG_INFO, "config loaded into NVRAM checker!\n"));
 
-  if (ret != 0)
-    DEBUG ((DEBUG_WARN, "%a returns %u %a!\n", __func__, ret, ret > 1 ? "errors" : "error"));
-  return ret;
+  ErrorCount = 0;
+
+  if (ErrorCount != 0) {
+    DEBUG ((DEBUG_WARN, "%a returns %u %a!\n", __func__, ErrorCount, ErrorCount > 1 ? "errors" : "error"));
+  }
+  return ErrorCount;
 }
 
-unsigned int check_PlatformInfo(OC_GLOBAL_CONFIG *Config) {
-  unsigned int ret = 0;
+STATIC
+UINT32
+CheckPlatformInfo (
+  IN  OC_GLOBAL_CONFIG  *Config
+  )
+{
+  UINT32 ErrorCount;
 
   DEBUG ((DEBUG_INFO, "config loaded into PlatformInfo checker!\n"));
 
-  if (ret != 0)
-    DEBUG ((DEBUG_WARN, "%a returns %u %a!\n", __func__, ret, ret > 1 ? "errors" : "error"));
-  return ret;
+  ErrorCount = 0;
+
+  if (ErrorCount != 0) {
+    DEBUG ((DEBUG_WARN, "%a returns %u %a!\n", __func__, ErrorCount, ErrorCount > 1 ? "errors" : "error"));
+  }
+  return ErrorCount;
 }
 
-unsigned int check_UEFI(OC_GLOBAL_CONFIG *Config) {
-  unsigned int ret = 0;
+STATIC
+UINT32
+CheckUEFI (
+  IN  OC_GLOBAL_CONFIG  *Config
+  )
+{
+  UINT32 ErrorCount = 0;
 
   DEBUG ((DEBUG_INFO, "config loaded into UEFI checker!\n"));
 
-  if (ret != 0)
-    DEBUG ((DEBUG_WARN, "%a returns %u %a!\n", __func__, ret, ret > 1 ? "errors" : "error"));
-  return ret;
+  ErrorCount = 0;
+
+  if (ErrorCount != 0) {
+    DEBUG ((DEBUG_WARN, "%a returns %u %a!\n", __func__, ErrorCount, ErrorCount > 1 ? "errors" : "error"));
+  }
+  return ErrorCount;
 }
 
-unsigned int (*checker_funcs[])(OC_GLOBAL_CONFIG *) = {
-  check_ACPI,
-  check_Booter,
-  check_DeviceProperties,
-  check_Kernel,
-  check_Misc,
-  check_NVRAM,
-  check_PlatformInfo,
-  check_UEFI
-};
-const size_t checker_funcs_size = sizeof(checker_funcs) / sizeof(checker_funcs[0]);
+STATIC
+UINT32
+CheckConfig (
+  IN  OC_GLOBAL_CONFIG  *Config
+  )
+{
+  UINT32  ErrorCount;
+  UINTN   Index;
+  UINTN   ConfigCheckersSize;
 
-unsigned int check_config(OC_GLOBAL_CONFIG *Config) {
-  unsigned int ret = 0;
+  ErrorCount = 0;
 
   //
-  // I don't think this is possible after OcConfigurationInit?
+  // TODO: Fix EDK II code style
   //
-  // if (!Config) {
-  //   DEBUG ((DEBUG_ERROR, "Failed to load config!\n"));
-  //   return 1;
-  // }
+  CONFIG_CHECK ConfigCheckers[] = {
+    &CheckACPI,
+    &CheckBooter,
+    &CheckDeviceProperties,
+    &CheckKernel,
+    &CheckMisc,
+    &CheckNVRAM,
+    &CheckPlatformInfo,
+    &CheckUEFI
+  };
+  ConfigCheckersSize = sizeof(ConfigCheckers) / sizeof(ConfigCheckers[0]);
 
-  for (size_t i = 0; i < checker_funcs_size; i++) {
-    ret |= (*checker_funcs[i])(Config);
+  for (Index = 0; Index < ConfigCheckersSize; ++Index) {
+    ErrorCount |= (*ConfigCheckers[Index]) (Config);
   }
 
-  return ret;
+  return ErrorCount;
 }
 
-int main(int argc, char** argv) {
-  uint32_t config_file_size;
-  uint8_t  *config_file_buffer;
-  if ((config_file_buffer = readFile(argc > 1 ? argv[1] : "config.plist", &config_file_size)) == NULL) {
-    DEBUG ((DEBUG_ERROR, "Failed to read %a\n", argc > 1 ? argv[1] : "config.plist"));
-    return -1;
-  }
-
-  long long a = current_timestamp();
+int main(int argc, const char *argv[]) {
+  UINT8              *ConfigFileBuffer;
+  UINT32             ConfigFileSize;
+  CONST CHAR8        *ConfigFileName;
+  INT64              ExecTime;
+  OC_GLOBAL_CONFIG   Config;
+  EFI_STATUS         Status;
+  UINT32             ErrorCount;
 
   PcdGet8  (PcdDebugPropertyMask)         |= DEBUG_PROPERTY_DEBUG_CODE_ENABLED;
   PcdGet32 (PcdFixedDebugPrintErrorLevel) |= DEBUG_INFO;
   PcdGet32 (PcdDebugPrintErrorLevel)      |= DEBUG_INFO;
 
-  OC_GLOBAL_CONFIG   Config;
-  EFI_STATUS         Status;
-  Status = OcConfigurationInit (&Config, config_file_buffer, config_file_size);
-
-  if (Status != EFI_SUCCESS) {
-    printf("Invalid config\n");
+  ConfigFileName   = argc > 1 ? argv[1] : "config.plist";
+  ConfigFileBuffer = readFile (ConfigFileName, &ConfigFileSize);
+  if (ConfigFileBuffer == NULL) {
+    DEBUG ((DEBUG_ERROR, "Failed to read %a\n", ConfigFileName));
     return -1;
   }
 
-  unsigned int ret = check_config(&Config);
-  if (ret == 0) {
-    DEBUG ((DEBUG_ERROR, "Done checking %a in %llu ms\n", argc > 1 ? argv[1] : "./config.plist", current_timestamp() - a));
+  ExecTime = GetCurrentTimestamp ();
+
+  Status   = OcConfigurationInit (&Config, ConfigFileBuffer, ConfigFileSize);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "Invalid config\n"));
+    return -1;
+  }
+
+  ErrorCount = CheckConfig (&Config);
+  if (ErrorCount == 0) {
+    DEBUG ((
+      DEBUG_ERROR,
+      "Done checking %a in %llu ms\n",
+      ConfigFileName,
+      GetCurrentTimestamp () - ExecTime
+      ));
   } else {
     DEBUG ((
       DEBUG_ERROR,
       "Done checking %a in %llu ms, but it has %u %a to be fixed\n",
-      argc > 1 ? argv[1] : "./config.plist",
-      current_timestamp() - a,
-      ret,
-      ret > 1 ? "errors" : "error"
+      ConfigFileName,
+      GetCurrentTimestamp () - ExecTime,
+      ErrorCount,
+      ErrorCount > 1 ? "errors" : "error"
       ));
   }
 
   OcConfigurationFree (&Config);
-  free(config_file_buffer);
+  free (ConfigFileBuffer);
 
   return 0;
 }
