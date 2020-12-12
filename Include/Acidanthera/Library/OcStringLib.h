ec85bc583dbf8370bb4cfba0e65b3702e79af0c7
PMheart <17109513+PMheart@users.noreply.github.com>
2020-12-13 00:41:39 +0800

    OcStringLib: Add IsAsciiAlpha API

--- Include/Acidanthera/Library/OcStringLib.h
+++ Include/Acidanthera/Library/OcStringLib.h
@@ -54,11 +54,22 @@ IsAsciiPrint (
   IN CHAR8  Char
   );
 
-/** Check if character is a white space character
+/** Check if character is alphabetical.
+
+  @param[in] Char  The ascii character to check if is alphabetical.
+
+  @retval  TRUE, if character is alphabetical.
+**/
+INTN
+IsAsciiAlpha (
+  IN CHAR8  Char
+  );
+
+/** Check if character is a white space character.
 
   @param[in] Char  The ascii character to check if is white space.
 
-  @retval  TRUE, if character is a white space character
+  @retval  TRUE, if character is a white space character.
 **/
 INTN
 IsAsciiSpace (
