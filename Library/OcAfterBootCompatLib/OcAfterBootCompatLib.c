acidanthera/OpenCorePkg

commit e2f219e07592ba1f0f1c763d68db1eb264d19fae

OcCpuLib: Fixed `FSBFrequency` calculation with fractional multiplier
closes acidanthera/bugtracker#1529

Library/OcAfterBootCompatLib/OcAfterBootCompatLib.c
@@ -96,7 +96,8 @@ GetBootCompatContext (
 
 EFI_STATUS
 OcAbcInitialize (
-  IN OC_ABC_SETTINGS  *Settings
+  IN OC_ABC_SETTINGS  *Settings,
+  IN OC_CPU_INFO      *CpuInfo
   )
 {
   EFI_STATUS           Status;

Include/Acidanthera/Library/OcAfterBootCompatLib.h
@@ -15,6 +15,8 @@
 #ifndef OC_AFTER_BOOT_COMPAT_LIB_H
 #define OC_AFTER_BOOT_COMPAT_LIB_H
 
+#include <Library/OcCpuLib.h>
+
 /**
   Booter patch structure.
 **/
@@ -196,12 +198,14 @@ typedef struct OC_ABC_SETTINGS_ {
   incompatible firmware to prevent boot failure and UEFI services breakage.
 
   @param[in]  Settings  Compatibility layer configuration.
+  @param[in]  CpuInfo   CPU information.
 
   @retval EFI_SUCCESS on success.
 **/
 EFI_STATUS
 OcAbcInitialize (
-  IN OC_ABC_SETTINGS  *Settings
+  IN OC_ABC_SETTINGS  *Settings,
+  IN OC_CPU_INFO      *CpuInfo
   );
 
 #endif // OC_AFTER_BOOT_COMPAT_LIB_H

Include/Acidanthera/Library/OcCpuLib.h
@@ -26,6 +26,31 @@
 #define OC_FALLBACK_CPU_FREQUENCY 1000000000
 #endif
 
+/**
+  Sorted Intel CPU generations.
+**/
+typedef enum {
+  OcCpuGenerationUnknown,
+  OcCpuGenerationBanias,
+  OcCpuGenerationPrePenryn,
+  OcCpuGenerationPenryn,
+  OcCpuGenerationNehalem,
+  OcCpuGenerationBonnel,
+  OcCpuGenerationWestmere,
+  OcCpuGenerationSandyBridge,
+  OcCpuGenerationPostSandyBridge,
+  OcCpuGenerationIvyBridge,
+  OcCpuGenerationHaswell,
+  OcCpuGenerationBroadwell,
+  OcCpuGenerationSkylake,
+  OcCpuGenerationKabyLake,
+  OcCpuGenerationCoffeeLake,
+  OcCpuGenerationCometLake,
+  OcCpuGenerationCannonLake,
+  OcCpuGenerationIceLake,
+  OcCpuGenerationMaxGeneration
+} OC_CPU_GENERATION;
+
 typedef struct {
   //
   // Note, Vendor and BrandString are reordered for proper alignment.
@@ -57,18 +82,10 @@ typedef struct {
   UINT16                      AppleProcessorType;
   BOOLEAN                     CstConfigLock;
 
+  OC_CPU_GENERATION           CpuGeneration;
+
   UINT32                      MaxId;
   UINT32                      MaxExtId;
-
-  UINT8                       MaxDiv;
-  UINT8                       CurBusRatio;  ///< Current Multiplier
-  UINT8                       MinBusRatio;  ///< Min Bus Ratio
-  UINT8                       MaxBusRatio;  ///< Max Bus Ratio
-
-  UINT8                       TurboBusRatio1;
-  UINT8                       TurboBusRatio2;
-  UINT8                       TurboBusRatio3;
-  UINT8                       TurboBusRatio4;
 
   UINT16                      PackageCount;
   UINT16                      CoreCount;
@@ -133,22 +150,6 @@ typedef struct {
   UINT64                      FSBFrequency;
 } OC_CPU_INFO;
 
-typedef enum {
-  OcCpuGenerationUnknown,
-  OcCpuGenerationPenryn,
-  OcCpuGenerationNehalem,
-  OcCpuGenerationWestmere,
-  OcCpuGenerationSandyBridge,
-  OcCpuGenerationIvyBridge,
-  OcCpuGenerationHaswell,
-  OcCpuGenerationBroadwell,
-  OcCpuGenerationSkylake,
-  OcCpuGenerationKabyLake,
-  OcCpuGenerationCoffeeLake,
-  OcCpuGenerationCannonLake,
-  OcCpuGenerationMaxGeneration
-} OC_CPU_GENERATION;
-
 /**
   Scan the processor and fill the cpu info structure with results.
 
@@ -201,16 +202,6 @@ OcCpuCorrectTscSync (
 UINT32
 OcCpuModelToAppleFamily (
   IN CPUID_VERSION_INFO_EAX  VersionEax
-  );
-
-/**
-  Obtain CPU's generation.
-
-  @retval CPU's generation (e.g. OcCpuGenerationUnknown).
- */
-OC_CPU_GENERATION
-OcCpuGetGeneration (
-  VOID
   );
 
 /**

Include/Acidanthera/Library/OcGuardLib.h
@@ -31,6 +31,25 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 #define OC_POT_ALIGNED(Align, Ptr) (0ULL == (((UINTN) (Ptr)) & (Align-1U)))
 #define OC_TYPE_ALIGNED(Type, Ptr) (OC_POT_ALIGNED (OC_ALIGNOF (Type), Ptr))
 
+/**
+ Return the result of (Multiplicand * Multiplier / Divisor).
+
+ @param Multiplicand A 64-bit unsigned value.
+ @param Multiplier   A 64-bit unsigned value.
+ @param Divisor      A 32-bit unsigned value.
+ @param Remainder    A pointer to a 32-bit unsigned value. This parameter is
+ optional and may be NULL.
+
+ @return Multiplicand * Multiplier / Divisor.
+ **/
+UINT64
+MultThenDivU64x64x32 (
+  IN  UINT64  Multiplicand,
+  IN  UINT64  Multiplier,
+  IN  UINT32  Divisor,
+  OUT UINT32  *Remainder  OPTIONAL
+  );
+
 //
 // The interfaces below provide base safe arithmetics, reporting
 // signed integer overflow and unsigned integer wraparound similarly to

Include/Acidanthera/Library/OcMiscLib.h
@@ -140,25 +140,6 @@ DirectResetCold (
   );
 
 /**
- Return the result of (Multiplicand * Multiplier / Divisor).
-
- @param Multiplicand A 64-bit unsigned value.
- @param Multiplier   A 64-bit unsigned value.
- @param Divisor      A 32-bit unsigned value.
- @param Remainder    A pointer to a 32-bit unsigned value. This parameter is
- optional and may be NULL.
-
- @return Multiplicand * Multiplier / Divisor.
- **/
-UINT64
-MultThenDivU64x64x32 (
-  IN  UINT64  Multiplicand,
-  IN  UINT64  Multiplier,
-  IN  UINT32  Divisor,
-  OUT UINT32  *Remainder  OPTIONAL
-  );
-
-/**
   Internal worker macro that calls DebugPrint().
 
   This macro calls DebugPrint(), passing in the filename, line number, an

Library/OcAfterBootCompatLib/BootCompatInternal.h
@@ -412,6 +412,10 @@ typedef struct BOOT_COMPAT_CONTEXT_ {
   /// Apple booter KASLR slide support internal state.
   ///
   SLIDE_SUPPORT_STATE      SlideSupport;
+  ///
+  /// CPU information.
+  ///
+  OC_CPU_INFO              *CpuInfo;
 } BOOT_COMPAT_CONTEXT;
 
 /**

Library/OcAfterBootCompatLib/CustomSlide.c
@@ -24,7 +24,6 @@
 #include <Library/BaseMemoryLib.h>
 #include <Library/DebugLib.h>
 #include <Library/OcBootManagementLib.h>
-#include <Library/OcCpuLib.h>
 #include <Library/OcCryptoLib.h>
 #include <Library/OcDeviceTreeLib.h>
 #include <Library/OcMachoLib.h>
@@ -229,7 +228,8 @@ ShouldUseCustomSlideOffset (
   IN OUT SLIDE_SUPPORT_STATE   *SlideSupport,
   IN     EFI_GET_MEMORY_MAP    GetMemoryMap       OPTIONAL,
   IN     OC_MEMORY_FILTER      FilterMap          OPTIONAL,
-  IN     VOID                  *FilterMapContext  OPTIONAL
+  IN     VOID                  *FilterMapContext  OPTIONAL,
+  IN     BOOLEAN               HasSandyOrIvy
   )
 {
   EFI_PHYSICAL_ADDRESS   AllocatedMapPages;
@@ -240,7 +240,6 @@ ShouldUseCustomSlideOffset (
   EFI_STATUS             Status;
   UINTN                  DescriptorSize;
   UINT32                 DescriptorVersion;
-  OC_CPU_GENERATION      CpuGeneration;
   UINTN                  Index;
   UINTN                  Slide;
   UINTN                  NumEntries;
@@ -280,9 +279,7 @@ ShouldUseCustomSlideOffset (
     FilterMap (FilterMapContext, MemoryMapSize, MemoryMap, DescriptorSize);
   }
 
-  CpuGeneration = OcCpuGetGeneration ();
-  SlideSupport->HasSandyOrIvy = CpuGeneration == OcCpuGenerationSandyBridge ||
-                                CpuGeneration == OcCpuGenerationIvyBridge;
+  SlideSupport->HasSandyOrIvy = HasSandyOrIvy;
 
   SlideSupport->EstimatedKernelArea = (UINTN) EFI_PAGES_TO_SIZE (
     OcCountRuntimePages (MemoryMapSize, MemoryMap, DescriptorSize, NULL)
@@ -807,7 +804,13 @@ AppleSlideGetVariable (
         );
     } else if (StrCmp (VariableName, L"boot-args") == 0
       && (!BootCompat->ServiceState.AppleCustomSlide || BootCompat->Settings.AllowRelocationBlock)
-      && ShouldUseCustomSlideOffset (&BootCompat->SlideSupport, GetMemoryMap, FilterMap, FilterMapContext)
+      && ShouldUseCustomSlideOffset (
+        &BootCompat->SlideSupport,
+        GetMemoryMap,
+        FilterMap,
+        FilterMapContext,
+        BootCompat->CpuInfo->CpuGeneration == OcCpuGenerationSandyBridge
+          || BootCompat->CpuInfo->CpuGeneration == OcCpuGenerationIvyBridge)
       && !BootCompat->ServiceState.AppleCustomSlide) {
       //
       // When we cannot allow some KASLR values due to used address we generate

Library/OcCpuLib/FrequencyDetect.c
@@ -24,7 +24,7 @@
 #include <Library/IoLib.h>
 #include <Library/OcCpuLib.h>
 #include <Library/PciLib.h>
-#include <Library/OcMiscLib.h>
+#include <Library/OcGuardLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/UefiRuntimeServicesTableLib.h>
 #include <IndustryStandard/ProcessorInfo.h>

commit bb6a7609389e6707dd253ad618bccdc11ecb190c

OcAfterBootCompatLib: Store BootCompat CpuInfo (#223)
Fixes a regression introduced in e2f219e07592ba1f0f1c763d68db1eb264d19fae

Library/OcAfterBootCompatLib/OcAfterBootCompatLib.c
@@ -156,6 +156,8 @@ OcAbcInitialize (
     sizeof (BootCompat->Settings)
     );
 
+  BootCompat->CpuInfo = CpuInfo;
+
   InstallServiceOverrides (BootCompat);
 
   return EFI_SUCCESS;