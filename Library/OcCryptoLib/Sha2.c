287322dcae7aecffb10c527e0ff4be52bef3d392
Mikhail Krichanov <krichanov@ispras.ru>
2021-04-10 10:27:47 +0300

    OcCryptoLib: Added Avx-acceleration of SHA512 and SHA384.

--- Library/OcCryptoLib/Sha2.c
+++ Library/OcCryptoLib/Sha2.c
@@ -36,31 +36,7 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 #endif
 
 #include <Library/OcCryptoLib.h>
-
-
-#define UNPACK64(x, str)                         \
-  do {                                           \
-    *((str) + 7) = (UINT8) (x);                  \
-    *((str) + 6) = (UINT8) RShiftU64 ((x),  8);  \
-    *((str) + 5) = (UINT8) RShiftU64 ((x), 16);  \
-    *((str) + 4) = (UINT8) RShiftU64 ((x), 24);  \
-    *((str) + 3) = (UINT8) RShiftU64 ((x), 32);  \
-    *((str) + 2) = (UINT8) RShiftU64 ((x), 40);  \
-    *((str) + 1) = (UINT8) RShiftU64 ((x), 48);  \
-    *((str) + 0) = (UINT8) RShiftU64 ((x), 56);  \
-  } while(0)
-
-#define PACK64(str, x)                           \
-  do {                                           \
-    *(x) =    ((UINT64) *((str) + 7))            \
-           | LShiftU64 (*((str) + 6),  8)        \
-           | LShiftU64 (*((str) + 5), 16)        \
-           | LShiftU64 (*((str) + 4), 24)        \
-           | LShiftU64 (*((str) + 3), 32)        \
-           | LShiftU64 (*((str) + 2), 40)        \
-           | LShiftU64 (*((str) + 1), 48)        \
-           | LShiftU64 (*((str) + 0), 56);       \
-  } while (0)
+#include "Sha2-avx.h"
 
 
 #define SHFR(a, b)    (a >> b)
@@ -141,26 +117,6 @@ STATIC UINT64 SHA512_K[80] = {
 STATIC CONST UINT32 SHA256_H0[8] = {
   0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
   0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
-};
-
-//
-// Sha 384 Init State
-//
-STATIC CONST UINT64 SHA384_H0[8] = {
-  0xcbbb9d5dc1059ed8ULL, 0x629a292a367cd507ULL,
-  0x9159015a3070dd17ULL, 0x152fecd8f70e5939ULL,
-  0x67332667ffc00b31ULL, 0x8eb44a8768581511ULL,
-  0xdb0c2e0d64f98fa7ULL, 0x47b5481dbefa4fa4ULL
-};
-
-//
-// Sha 512 Init State
-//
-STATIC CONST UINT64 SHA512_H0[8] = {
-  0x6a09e667f3bcc908ULL, 0xbb67ae8584caa73bULL,
-  0x3c6ef372fe94f82bULL, 0xa54ff53a5f1d36f1ULL,
-  0x510e527fade682d1ULL, 0x9b05688c2b3e6c1fULL,
-  0x1f83d9abfb41bd6bULL, 0x5be0cd19137e2179ULL
 };
 
 //
@@ -494,6 +450,11 @@ Sha512 (
 {
   SHA512_CONTEXT  Ctx;
 
+  if (IsAvxSupported ()) {
+    Sha512Avx (Hash, Data, Len);
+    return;
+  }
+
   Sha512Init (&Ctx);
   Sha512Update (&Ctx, Data, Len);
   Sha512Final (&Ctx, Hash);
@@ -598,6 +559,11 @@ Sha384 (
   )
 {
   SHA384_CONTEXT Ctx;
+
+  if (IsAvxSupported ()) {
+    Sha384Avx (Hash, Data, Len);
+    return;
+  }
 
   Sha384Init (&Ctx);
   Sha384Update (&Ctx, Data, Len);
