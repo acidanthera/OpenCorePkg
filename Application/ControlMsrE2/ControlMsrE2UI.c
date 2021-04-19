e7af0940c2ce813cbf5c6c99419000965e8664af
zhen-zen <66577170+zhen-zen@users.noreply.github.com>
2021-04-19 05:03:40 -0700

    Tools: support case insensitive arguments (#254)

--- Application/ControlMsrE2/ControlMsrE2UI.c
+++ Application/ControlMsrE2/ControlMsrE2UI.c
@@ -181,11 +181,11 @@ InterpretArguments (
 
       Token[TokenIndex] = '\0';
 
-      if (StrCmp (Parameter, L"lock") == 0) {
+      if (OcStriCmp (Parameter, L"lock") == 0) {
         mArgumentFlags = ARG_LOCK;
-      } else if (StrCmp (Parameter, L"unlock") == 0) {
+      } else if (OcStriCmp (Parameter, L"unlock") == 0) {
         mArgumentFlags = ARG_UNLOCK;
-      } else if (StrCmp (Parameter, L"interactive") == 0) {
+      } else if (OcStriCmp (Parameter, L"interactive") == 0) {
         mArgumentFlags = ARG_INTERACTIVE;
       } else {
         Print (L"Unknown command line argument: %s\n", Parameter);
