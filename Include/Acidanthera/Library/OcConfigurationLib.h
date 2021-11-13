12a6381ab9bbd487dc038255354bfbd35fae09e7
PMheart <17109513+PMheart@users.noreply.github.com>
2021-11-13 19:40:53 +0100

    OcConfigurationLib: Initial structure commit of LogModule

--- Include/Acidanthera/Library/OcConfigurationLib.h
+++ Include/Acidanthera/Library/OcConfigurationLib.h
@@ -352,7 +352,8 @@
   _(BOOLEAN                     , ApplePanic                  ,     , FALSE        , ()) \
   _(BOOLEAN                     , DisableWatchDog             ,     , FALSE        , ()) \
   _(BOOLEAN                     , SerialInit                  ,     , FALSE        , ()) \
-  _(BOOLEAN                     , SysReport                   ,     , FALSE        , ())
+  _(BOOLEAN                     , SysReport                   ,     , FALSE        , ()) \
+  _(OC_STRING                   , LogModule                   ,     , OC_STRING_CONSTR ("*", _, __) , OC_DESTR (OC_STRING))
   OC_DECLARE (OC_MISC_DEBUG)
 
 #define OCS_EXPOSE_BOOT_PATH   1U
