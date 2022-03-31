4c41dd3e1db0db3996735b07938acb51d1391dbb
PMheart <17109513+PMheart@users.noreply.github.com>
2022-03-31 14:38:43 +0200

    ocvalidate: Move PCD value sanity checks to ocvalidate

--- Utilities/ocvalidate/ValidateMisc.c
+++ Utilities/ocvalidate/ValidateMisc.c
@@ -567,6 +567,79 @@ CheckMiscSecurity (
 }
 
 STATIC
+BOOLEAN
+ValidateBaudRate (
+  IN  UINT32  BaudRate
+  )
+{
+  UINTN  Index;
+
+  STATIC UINT32  AllowedBaudRate[] = {
+    921600U, 460800U, 230400U, 115200U,
+    57600U, 38400U, 19200U, 9600U, 7200U, 
+    4800U, 3600U, 2400U, 2000U, 1800U,
+    1200U, 600U, 300U, 150U, 134U, 
+    110U, 75U, 50U
+  };
+
+  for (Index = 0; Index < ARRAY_SIZE (AllowedBaudRate); ++Index) {
+    if (BaudRate == AllowedBaudRate[Index]) {
+      return TRUE;
+    }
+  }
+
+  DEBUG ((DEBUG_WARN, "Misc->Serial->BaudRate is borked!\n"));
+  DEBUG ((DEBUG_WARN, "Accepted BaudRate values:\n"));
+  for (Index = 0; Index < ARRAY_SIZE (AllowedBaudRate); ++Index) {
+    DEBUG ((DEBUG_WARN, "%u, ", AllowedBaudRate[Index]));
+    if (Index != 0 && Index % 5 == 0) {
+      DEBUG ((DEBUG_WARN, "\n"));
+    }
+  }
+  DEBUG ((DEBUG_WARN, "\n"));
+  return FALSE;
+}
+
+STATIC
+UINT32
+CheckMiscSerial (
+  IN  OC_GLOBAL_CONFIG  *Config
+  )
+{
+  UINT32            ErrorCount;
+  OC_MISC_CONFIG    *UserMisc;
+  UINT32            RegisterAccessWidth;
+  UINT32            BaudRate;
+  UINT32            PciDeviceInfoSize;
+
+  ErrorCount        = 0;
+  UserMisc          = &Config->Misc;
+
+  RegisterAccessWidth = UserMisc->Serial.RegisterAccessWidth;
+  if (RegisterAccessWidth != 8U && RegisterAccessWidth != 32U) {
+    DEBUG ((DEBUG_WARN, "Misc->Serial->RegisterAccessWidth can only be 8 or 32!\n"));
+    ++ErrorCount;
+  }
+
+  BaudRate = UserMisc->Serial.BaudRate;
+  if (!ValidateBaudRate (BaudRate)) {
+    ++ErrorCount;
+  }
+
+  //
+  // Reference:
+  // https://github.com/acidanthera/bugtracker/issues/1954#issuecomment-1084220743
+  //
+  PciDeviceInfoSize = UserMisc->Serial.PciDeviceInfo.Size;
+  if (PciDeviceInfoSize <= 41U) {
+    DEBUG ((DEBUG_WARN, "Size of Misc->Serial->RegisterAccessWidth cannot exceed 41!\n"));
+    ++ErrorCount;
+  }
+
+  return ErrorCount;
+}
+
+STATIC
 UINT32
 CheckMiscTools (
   IN  OC_GLOBAL_CONFIG  *Config
@@ -662,6 +735,7 @@ CheckMisc (
     &CheckMiscDebug,
     &CheckMiscEntries,
     &CheckMiscSecurity,
+    &CheckMiscSerial,
     &CheckMiscTools
   };
 
