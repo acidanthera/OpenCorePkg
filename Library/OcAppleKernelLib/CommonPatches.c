6fc0fe6f0a0a30a23c6a91e24d8ce6c5a657f026
PMheart <17109513+PMheart@users.noreply.github.com>
2022-04-13 02:55:58 +0200

    OcAppleKernelLib: Fix apfs timeout patch

--- Library/OcAppleKernelLib/CommonPatches.c
+++ Library/OcAppleKernelLib/CommonPatches.c
@@ -2275,8 +2275,7 @@ PatchSetApfsTrimTimeout (
   //
   // Disable trim using another patch when timeout is 0.
   //
-  if (mApfsTimeoutReplace[2] == 0 && mApfsTimeoutReplace[3] == 0
-    && mApfsTimeoutReplace[4] == 0 && mApfsTimeoutReplace[5] == 0) {
+  if (IsZeroBuffer (&mApfsDisableTrimPatch[2]), sizeof (UINT32)) {
     Status = PatcherApplyGenericPatch (Patcher, &mApfsDisableTrimPatch);
     if (EFI_ERROR (Status)) {
       DEBUG ((DEBUG_INFO, "OCAK: Failed to apply patch ApfsDisableTrim - %r\n", Status));
