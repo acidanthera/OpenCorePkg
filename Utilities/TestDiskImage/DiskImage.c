6290f970ecd927be6674dc17d507a9a2272c843b
PMheart <17109513+PMheart@users.noreply.github.com>
2022-04-22 08:16:02 +0200

    TestDiskImage: Convert to EDK-II codestyle

--- Utilities/TestDiskImage/DiskImage.c
+++ Utilities/TestDiskImage/DiskImage.c
@@ -3,8 +3,6 @@
   SPDX-License-Identifier: BSD-3-Clause
 **/
 
-#include "../Include/Uefi.h"
-
 #include <Library/OcAppleChunklistLib.h>
 #include <Library/OcAppleDiskImageLib.h>
 #include <Library/OcAppleRamDiskLib.h>
@@ -13,167 +11,168 @@
 #include <Library/MemoryAllocationLib.h>
 #include <Library/DebugLib.h>
 
-#include <string.h>
-
 #include <UserFile.h>
 
-int ENTRY_POINT (int argc, char *argv[]) {
+#define  NUM_EXTENTS  20
+
+int ENTRY_POINT (int argc, const char *argv[]) {
+  int                          Index;
+  BOOLEAN                      DmgContextValid;
+  UINT8                        *Dmg;
+  UINT32                       DmgSize;
+  UINT8                        *Chunklist;
+  UINT32                       ChunklistSize;
+  UINT8                        *UncompDmg;
+  UINT32                       UncompSize;
+  BOOLEAN                      Result;
+  OC_APPLE_DISK_IMAGE_CONTEXT  DmgContext;
+  APPLE_RAM_DISK_EXTENT_TABLE  ExtentTable;
+  UINT32                       Index2;
+  OC_APPLE_CHUNKLIST_CONTEXT   ChunklistContext;
+
   if (argc < 2) {
-    printf ("Please provide a valid Disk Image path\n");
+    DEBUG ((DEBUG_ERROR, "Please provide a valid Disk Image path\n"));
     return -1;
   }
   
   if ((argc % 2) != 1) {
-    printf ("Please provide a chunklist file for each DMG, enter \'n\' to skip\n");
+    DEBUG ((DEBUG_ERROR, "Please provide a chunklist file for each DMG, enter \'n\' to skip\n"));
   }
 
-  for (int i = 1; i < (argc - 1); i+=2) {
-    int     DmgContextValid = 0;
-    uint8_t  *Dmg = NULL;
-    uint32_t DmgSize;
+  for (Index = 1; Index < (argc - 1); Index += 2) {
+    DmgContextValid = FALSE;
+    Dmg             = NULL;
+    Chunklist       = NULL;
+    UncompDmg       = NULL;
 
-    uint8_t  *Chunklist = NULL;
-    uint32_t ChunklistSize;
-
-    uint8_t  *UncompDmg = NULL;
-    uint32_t UncompSize;
-
-    if ((Dmg = UserReadFile (argv[i], &DmgSize)) == NULL) {
-      printf ("Read fail\n");
+    if ((Dmg = UserReadFile (argv[Index], &DmgSize)) == NULL) {
+      DEBUG ((DEBUG_ERROR, "Read fail\n"));
       goto ContinueDmgLoop;
     }
-
-    BOOLEAN Result;
-
-    OC_APPLE_DISK_IMAGE_CONTEXT DmgContext;
-    APPLE_RAM_DISK_EXTENT_TABLE ExtentTable;
-
-#define NUM_EXTENTS 20
 
     ExtentTable.Signature   = APPLE_RAM_DISK_EXTENT_SIGNATURE;
     ExtentTable.Version     = APPLE_RAM_DISK_EXTENT_VERSION;
     ExtentTable.Reserved    = 0;
     ExtentTable.Signature2  = APPLE_RAM_DISK_EXTENT_SIGNATURE;
-
     ExtentTable.ExtentCount = MIN (NUM_EXTENTS, ARRAY_SIZE (ExtentTable.Extents));
 
-    UINT32 Index;
-    for (Index = 0; Index < ExtentTable.ExtentCount; ++Index) {
-      ExtentTable.Extents[Index].Start = (uintptr_t)Dmg + (Index * (DmgSize / ExtentTable.ExtentCount));
-      ExtentTable.Extents[Index].Length = (DmgSize / ExtentTable.ExtentCount);
+    for (Index2 = 0; Index2 < ExtentTable.ExtentCount; ++Index2) {
+      ExtentTable.Extents[Index2].Start = (UINTN) Dmg + (Index2 * (DmgSize / ExtentTable.ExtentCount));
+      ExtentTable.Extents[Index2].Length = (DmgSize / ExtentTable.ExtentCount);
     }
-    if (Index != 0) {
-      ExtentTable.Extents[Index - 1].Length += (DmgSize - (Index * (DmgSize / ExtentTable.ExtentCount)));
+    if (Index2 != 0) {
+      ExtentTable.Extents[Index2 - 1].Length += (DmgSize - (Index2 * (DmgSize / ExtentTable.ExtentCount)));
     }
 
     Result = OcAppleDiskImageInitializeContext (&DmgContext, &ExtentTable, DmgSize);
     if (!Result) {
-      printf ("DMG Context initialization error\n");
+      DEBUG ((DEBUG_ERROR, "DMG Context initialization error\n"));
       goto ContinueDmgLoop;
     }
 
-    DmgContextValid = 1;
+    DmgContextValid = TRUE;
 
-    if (strcmp (argv[i + 1], "n") != 0) {
-      if ((Chunklist = UserReadFile (argv[i + 1], &ChunklistSize)) == NULL) {
-        printf ("Read fail\n");
+    if (AsciiStrCmp (argv[Index + 1], "n") != 0) {
+      if ((Chunklist = UserReadFile (argv[Index + 1], &ChunklistSize)) == NULL) {
+        DEBUG ((DEBUG_ERROR, "Read fail\n"));
         goto ContinueDmgLoop;
       }
 
-      OC_APPLE_CHUNKLIST_CONTEXT ChunklistContext;
       Result = OcAppleChunklistInitializeContext (&ChunklistContext, Chunklist, ChunklistSize);
       if (!Result) {
-        printf ("Chunklist Context initialization error\n");
+        DEBUG ((DEBUG_ERROR, "Chunklist Context initialization error\n"));
         goto ContinueDmgLoop;
       }
 
       Result = OcAppleChunklistVerifySignature (&ChunklistContext, PkDataBase[0].PublicKey);
       if (!Result) {
-        printf ("Chunklist signature verification error\n");
+        DEBUG ((DEBUG_ERROR, "Chunklist signature verification error\n"));
         goto ContinueDmgLoop;
       }
 
       Result = OcAppleDiskImageVerifyData (&DmgContext, &ChunklistContext);
       if (!Result) {
-        printf ("Chunklist chunk verification error\n");
+        DEBUG ((DEBUG_ERROR, "Chunklist chunk verification error\n"));
         goto ContinueDmgLoop;
       }
     }
 
     UncompSize = (DmgContext.SectorCount * APPLE_DISK_IMAGE_SECTOR_SIZE);
-    UncompDmg  = malloc (UncompSize);
+    UncompDmg  = AllocatePool (UncompSize);
     if (UncompDmg == NULL) {
-      printf ("DMG data allocation failed\n");
+      DEBUG ((DEBUG_ERROR, "DMG data allocation failed\n"));
       goto ContinueDmgLoop;
     }
 
     Result = OcAppleDiskImageRead (&DmgContext, 0, UncompSize, UncompDmg);
     if (!Result) {
-      printf ("DMG read error\n");
+      DEBUG ((DEBUG_ERROR, "DMG read error\n"));
       goto ContinueDmgLoop;
     }
 
-    printf ("Decompressed the entire DMG...\n");
+    DEBUG ((DEBUG_ERROR, "Decompressed the entire DMG...\n"));
 
 #if 0
-    FILE *Fh = fopen("out.bin", "wb");
-    if (Fh != NULL) {
-      fwrite (UncompDmg, UncompSize, 1, Fh);
-      fclose (Fh);
-    } else {
-      printf("File error\n");
-    }
+    UserWriteFile ("out.bin", UncompDmg, UncompSize);
 #endif
 
 #if 0
-    unsigned char Digest[CC_MD5_DIGEST_LENGTH];
-    CC_MD5_CTX Context;
+    UINT8       Digest[CC_MD5_DIGEST_LENGTH];
+    CC_MD5_CTX  Context;
+    UINTN       Index3;
+
     CC_MD5_Init (&Context);
     CC_MD5_Update (&Context, UncompDmg, UncompSize);
     CC_MD5_Final (Digest, &Context);
-    for (size_t i = 0; i < CC_MD5_DIGEST_LENGTH; ++i)
-      printf("%02x", (unsigned int) Digest[i]);
-    puts("");
+    for (Index3 = 0; Index3 < CC_MD5_DIGEST_LENGTH; ++Index3) {
+      DEBUG ((DEBUG_ERROR, "%02x", (UINT32) Digest[Index3]));
+    }
+    DEBUG ((DEBUG_ERROR, "\n"));
 #endif
 
-    printf ("Success...\n");
+    DEBUG ((DEBUG_ERROR, "Success...\n"));
 
   ContinueDmgLoop:
     if (DmgContextValid) {
       OcAppleDiskImageFreeContext (&DmgContext);
     }
 
-    free (Dmg);
-    free (Chunklist);
-    free (UncompDmg);
+    FreePool (Dmg);
+    FreePool (Chunklist);
+    FreePool (UncompDmg);
   }
 
   return 0;
 }
 
 INT32 LLVMFuzzerTestOneInput(CONST UINT8 *Data, UINTN Size) {
-  #define MAX_INPUT 256
-  #define MAX_OUTPUT 4096
+  #define  MAX_INPUT   256
+  #define  MAX_OUTPUT  4096
+
+  UINT8   *Test;
+  UINTN   Index;
+  UINT32  CurrentLength;
 
   if (Size > MAX_INPUT) {
     return 0;
   }
 
-  UINT8 *Test = AllocateZeroPool (MAX_OUTPUT);
+  Test = AllocateZeroPool (MAX_OUTPUT);
   if (Test == NULL) {
     return 0;
   }
 
-  for (size_t Index = 0; Index < 4096; ++Index) {
+  for (Index = 0; Index < 4096; ++Index) {
     ASAN_POISON_MEMORY_REGION (Test + Index, MAX_OUTPUT - Index);
-    UINT32 CurrentLength = DecompressZLIB (
-                           Test,
-                           Index,
-                           Data,
-                           Size
-                           );
+    CurrentLength = DecompressZLIB (
+                      Test,
+                      Index,
+                      Data,
+                      Size
+                      );
     ASAN_UNPOISON_MEMORY_REGION (Test + Index, MAX_OUTPUT - Index);
     ASSERT (CurrentLength <= Index);
   }
+  
   return 0;
 }
