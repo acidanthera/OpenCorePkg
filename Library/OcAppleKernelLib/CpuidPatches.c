acidanthera/OpenCorePkg

commit d916dd65b8cad8647101cddef2f38efd071ebbbd

OcMachoLib: Treat container Mach-O as reference file
As of macOS 13 Developer Beta 3, the Kernel Collection's inner kernel
references a segment that precedes itself. The current model is that
a Kernel Collection is a container format and the included files are
(mostly) separate. Hence, this was treated as an out-of-bounds issue.
Kernel Collections apparently are rather an unconventional composite
format, where the sub-files are still part of the whole. Redesign
OcMachoLib to treat the Kernel Collection as the reference file.
Patches still use only the inner file, while parsing considers the
whole file.

Library/OcAppleKernelLib/CpuidPatches.c
@@ -815,7 +815,7 @@ PatchKernelCpuId (
     );
 
   Start = ((UINT8 *)MachoGetMachHeader (&Patcher->MachContext));
-  Last  = Start + MachoGetFileSize (&Patcher->MachContext) - EFI_PAGE_SIZE * 2 - sizeof (mKernelCpuIdFindRelNew);
+  Last  = Start + MachoGetInnerSize (&Patcher->MachContext) - EFI_PAGE_SIZE * 2 - sizeof (mKernelCpuIdFindRelNew);
 
   //
   // Do legacy patching for 32-bit 10.7, and 10.6 and older.

Include/Acidanthera/Library/OcAppleKernelLib.h
@@ -1004,16 +1004,18 @@ KcGetKextSize (
 /**
   Apply the delta from KC header to the file's offsets.
 
-  @param[in,out] Context  The context of the KEXT to rebase.
-  @param[in]     Delta    The offset from KC header the KEXT starts at.
+  @param[in]     PrelinkedContext  Prelinked context.
+  @param[in,out] Context           The context of the KEXT to rebase.
+  @param[in]     Delta             The offset from KC header the KEXT starts at.
 
   @retval EFI_SUCCESS  The file has beem rebased successfully.
   @retval other        An error has occured.
 **/
 EFI_STATUS
 KcKextApplyFileDelta (
-  IN OUT OC_MACHO_CONTEXT  *Context,
-  IN     UINT32            Delta
+  IN     PRELINKED_CONTEXT  *PrelinkedContext,
+  IN OUT OC_MACHO_CONTEXT   *Context,
+  IN     UINT32             Delta
   );
 
 /**

Include/Acidanthera/Library/OcMachoLib.h
@@ -32,10 +32,11 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 /// only.  Members are not guaranteed to be sane.
 ///
 typedef struct {
-  MACH_HEADER_ANY          *MachHeader;
+  VOID                     *FileData;
   UINT32                   FileSize;
 
-  UINT32                   ContainerOffset;
+  MACH_HEADER_ANY          *MachHeader;
+  UINT32                   InnerSize;
   MACH_SYMTAB_COMMAND      *Symtab;
   MACH_NLIST_ANY           *SymbolTable;
   CHAR8                    *StringTable;
@@ -50,11 +51,12 @@ typedef struct {
 /**
   Initializes a 32-bit Mach-O Context.
 
-  @param[out] Context          Mach-O Context to initialize.
-  @param[in]  FileData         Pointer to the file's expected Mach-O header.
-  @param[in]  FileSize         File size of FileData.
-  @param[in]  ContainerOffset  The amount of Bytes the Mach-O header is offset
-                               from the base (container, e.g. KC) of the file.
+  @param[out] Context       Mach-O Context to initialize.
+  @param[in]  FileData      Pointer to the file's expected Mach-O header.
+  @param[in]  FileSize      File size of FileData.
+  @param[in]  HeaderOffset  The amount of Bytes the Mach-O header is offset from
+                            the base (container, e.g. KC) of the file.
+  @param[in]  InnerSize     The size, in Bytes, of the inner Mach-O file.
 
   @return  Whether Context has been initialized successfully.
 
@@ -64,17 +66,19 @@ MachoInitializeContext32 (
   OUT OC_MACHO_CONTEXT  *Context,
   IN  VOID              *FileData,
   IN  UINT32            FileSize,
-  IN  UINT32            ContainerOffset
+  IN  UINT32            HeaderOffset,
+  IN  UINT32            InnerSize
   );
 
 /**
   Initializes a 64-bit Mach-O Context.
 
-  @param[out] Context          Mach-O Context to initialize.
-  @param[in]  FileData         Pointer to the file's expected Mach-O header.
-  @param[in]  FileSize         File size of FileData.
-  @param[in]  ContainerOffset  The amount of Bytes the Mach-O header is offset
-                               from the base (container, e.g. KC) of the file.
+  @param[out] Context       Mach-O Context to initialize.
+  @param[in]  FileData      Pointer to the file's expected Mach-O header.
+  @param[in]  FileSize      File size of FileData.
+  @param[in]  HeaderOffset  The amount of Bytes the Mach-O header is offset from
+                            the base (container, e.g. KC) of the file.
+  @param[in]  InnerSize     The size, in Bytes, of the inner Mach-O file.
 
   @return  Whether Context has been initialized successfully.
 
@@ -84,18 +88,20 @@ MachoInitializeContext64 (
   OUT OC_MACHO_CONTEXT  *Context,
   IN  VOID              *FileData,
   IN  UINT32            FileSize,
-  IN  UINT32            ContainerOffset
+  IN  UINT32            HeaderOffset,
+  IN  UINT32            InnerSize
   );
 
 /**
   Initializes a Mach-O Context.
 
-  @param[out] Context          Mach-O Context to initialize.
-  @param[in]  FileData         Pointer to the file's expected Mach-O header.
-  @param[in]  FileSize         File size of FileData.
-  @param[in]  ContainerOffset  The amount of Bytes the Mach-O header is offset
-                               from the base (container, e.g. KC) of the file.
-  @param[in]  Is32Bit          TRUE if Mach-O is 32-bit.
+  @param[out] Context       Mach-O Context to initialize.
+  @param[in]  FileData      Pointer to the file's expected Mach-O header.
+  @param[in]  FileSize      File size of FileData.
+  @param[in]  HeaderOffset  The amount of Bytes the Mach-O header is offset from
+                            the base (container, e.g. KC) of the file.
+  @param[in]  InnerSize     The size, in Bytes, of the inner Mach-O file.
+  @param[in]  Is32Bit       TRUE if Mach-O is 32-bit.
 
   @return  Whether Context has been initialized successfully.
 
@@ -105,7 +111,8 @@ MachoInitializeContext (
   OUT OC_MACHO_CONTEXT  *Context,
   IN  VOID              *FileData,
   IN  UINT32            FileSize,
-  IN  UINT32            ContainerOffset,
+  IN  UINT32            HeaderOffset,
+  IN  UINT32            InnerSize,
   IN  BOOLEAN           Is32Bit
   );
 
@@ -117,6 +124,17 @@ MachoInitializeContext (
 **/
 MACH_HEADER_ANY *
 MachoGetMachHeader (
+  IN OUT OC_MACHO_CONTEXT  *Context
+  );
+
+/**
+  Returns the size of the inner Mach-O file (otherwise, the file size).
+
+  @param[in,out] Context  Context of the Mach-O.
+
+**/
+UINT32
+MachoGetInnerSize (
   IN OUT OC_MACHO_CONTEXT  *Context
   );
 
@@ -139,6 +157,17 @@ MachoGetMachHeader32 (
 **/
 MACH_HEADER_64 *
 MachoGetMachHeader64 (
+  IN OUT OC_MACHO_CONTEXT  *Context
+  );
+
+/**
+  Returns the file data of the Mach-O.
+
+  @param[in,out] Context  Context of the Mach-O.
+
+**/
+VOID *
+MachoGetFileData (
   IN OUT OC_MACHO_CONTEXT  *Context
   );
 

Library/OcAppleKernelLib/CommonPatches.c
@@ -77,7 +77,7 @@ PatchAppleCpuPmCfgLock (
 
   Count     = 0;
   Walker    = (UINT8 *)MachoGetMachHeader (&Patcher->MachContext);
-  WalkerEnd = Walker + MachoGetFileSize (&Patcher->MachContext) - mWrmsrMaxDistance;
+  WalkerEnd = Walker + MachoGetInnerSize (&Patcher->MachContext) - mWrmsrMaxDistance;
 
   //
   // Thanks to Clover developers for the approach.
@@ -254,7 +254,7 @@ PatchAppleXcpmCfgLock (
   }
 
   Last = (XCPM_MSR_RECORD *)((UINT8 *)MachoGetMachHeader (&Patcher->MachContext)
-                             + MachoGetFileSize (&Patcher->MachContext) - sizeof (XCPM_MSR_RECORD));
+                             + MachoGetInnerSize (&Patcher->MachContext) - sizeof (XCPM_MSR_RECORD));
 
   Replacements = 0;
 
@@ -381,7 +381,7 @@ PatchAppleXcpmExtraMsrs (
   }
 
   Last = (XCPM_MSR_RECORD *)((UINT8 *)MachoGetMachHeader (&Patcher->MachContext)
-                             + MachoGetFileSize (&Patcher->MachContext) - sizeof (XCPM_MSR_RECORD));
+                             + MachoGetInnerSize (&Patcher->MachContext) - sizeof (XCPM_MSR_RECORD));
 
   Replacements = 0;
 
@@ -509,7 +509,7 @@ PatchAppleXcpmForceBoost (
   }
 
   Start   = (UINT8 *)MachoGetMachHeader (&Patcher->MachContext);
-  Last    = Start + MachoGetFileSize (&Patcher->MachContext) - EFI_PAGE_SIZE * 2;
+  Last    = Start + MachoGetInnerSize (&Patcher->MachContext) - EFI_PAGE_SIZE * 2;
   Start  += EFI_PAGE_SIZE;
   Current = Start;
 
@@ -1190,7 +1190,7 @@ PatchCustomPciSerialPmio (
 
   Count     = 0;
   Walker    = (UINT8 *)MachoGetMachHeader (&Patcher->MachContext);
-  WalkerEnd = Walker + MachoGetFileSize (&Patcher->MachContext) - mInOutMaxDistance;
+  WalkerEnd = Walker + MachoGetInnerSize (&Patcher->MachContext) - mInOutMaxDistance;
 
   while (Walker < WalkerEnd) {
     if (  (Walker[0] == mSerialDevicePmioFind[0])
@@ -1385,7 +1385,7 @@ PatchPanicKextDump (
   }
 
   Last = ((UINT8 *)MachoGetMachHeader (&Patcher->MachContext)
-          + MachoGetFileSize (&Patcher->MachContext) - EFI_PAGE_SIZE);
+          + MachoGetInnerSize (&Patcher->MachContext) - EFI_PAGE_SIZE);
 
   //
   // This should work on 10.15 and all debug kernels.
@@ -1811,7 +1811,7 @@ PatchSegmentJettison (
   }
 
   Last = (UINT8 *)MachoGetMachHeader (&Patcher->MachContext)
-         + MachoGetFileSize (&Patcher->MachContext) - sizeof (EFI_PAGE_SIZE) * 2;
+         + MachoGetInnerSize (&Patcher->MachContext) - sizeof (EFI_PAGE_SIZE) * 2;
 
   Status = PatcherGetSymbolAddress (Patcher, "__ZN6OSKext19removeKextBootstrapEv", (UINT8 **)&RemoveBs);
   if (EFI_ERROR (Status) || (RemoveBs > Last)) {
@@ -2055,7 +2055,7 @@ PatchLegacyCommpage (
   ASSERT (Patcher != NULL);
 
   Start = ((UINT8 *)MachoGetMachHeader (&Patcher->MachContext));
-  Last  = Start + MachoGetFileSize (&Patcher->MachContext) - EFI_PAGE_SIZE * 2 - (Patcher->Is32Bit ? sizeof (COMMPAGE_DESCRIPTOR) : sizeof (COMMPAGE_DESCRIPTOR_64));
+  Last  = Start + MachoGetInnerSize (&Patcher->MachContext) - EFI_PAGE_SIZE * 2 - (Patcher->Is32Bit ? sizeof (COMMPAGE_DESCRIPTOR) : sizeof (COMMPAGE_DESCRIPTOR_64));
 
   //
   // This is a table of pointers to commpage entries.
@@ -2294,7 +2294,7 @@ PatchForceSecureBootScheme (
   //
 
   Last = ((UINT8 *)MachoGetMachHeader (&Patcher->MachContext)
-          + MachoGetFileSize (&Patcher->MachContext) - 64);
+          + MachoGetInnerSize (&Patcher->MachContext) - 64);
 
   Status = PatcherGetSymbolAddress (Patcher, "_img4_chip_select_effective_ap", &SelectAp);
   if (EFI_ERROR (Status) || (SelectAp > Last)) {

Library/OcAppleKernelLib/KernelCollection.c
@@ -565,6 +565,7 @@ KcKextIndexFixups (
   CONST MACH_SEGMENT_COMMAND_64  *FirstSegment;
   MACH_HEADER_64                 *MachHeader;
   CONST MACH_RELOCATION_INFO     *Relocations;
+  VOID                           *FileData;
   UINT32                         RelocIndex;
 
   ASSERT (Context != NULL);
@@ -616,8 +617,9 @@ KcKextIndexFixups (
   //
   // Convert all relocations to fixups.
   //
+  FileData    = MachoGetFileData (MachContext);
   Relocations = (MACH_RELOCATION_INFO *)(
-                                         (UINTN)MachHeader + DySymtab->LocalRelocationsOffset
+                                         (UINTN)FileData + DySymtab->LocalRelocationsOffset
                                          );
 
   DEBUG ((
@@ -643,14 +645,11 @@ KcGetKextSize (
   IN UINT64             SourceAddress
   )
 {
-  MACH_HEADER_64           *KcHeader;
   MACH_SEGMENT_COMMAND_64  *Segment;
 
   ASSERT (Context != NULL);
   ASSERT (Context->IsKernelCollection);
 
-  KcHeader = MachoGetMachHeader64 (&Context->PrelinkedMachContext);
-  ASSERT (KcHeader != NULL);
   //
   // Find the KC segment that contains the KEXT at SourceAddress.
   //
@@ -683,8 +682,9 @@ KcGetKextSize (
 
 EFI_STATUS
 KcKextApplyFileDelta (
-  IN OUT OC_MACHO_CONTEXT  *Context,
-  IN     UINT32            Delta
+  IN     PRELINKED_CONTEXT  *PrelinkedContext,
+  IN OUT OC_MACHO_CONTEXT   *Context,
+  IN     UINT32             Delta
   )
 {
   MACH_HEADER_64           *KextHeader;
@@ -695,6 +695,7 @@ KcKextApplyFileDelta (
   MACH_DYSYMTAB_COMMAND    *DySymtab;
   UINT32                   SectIndex;
 
+  ASSERT (PrelinkedContext != NULL);
   ASSERT (Context != NULL);
   ASSERT (Delta > 0);
 
@@ -786,9 +787,11 @@ KcKextApplyFileDelta (
 
   //
   // Update the container offset to make sure we can link against this
-  // kext later as well.
+  // kext later as well. Context->InnerSize remains unchanged and is the actual
+  // size of the kext.
   //
-  Context->ContainerOffset = Delta;
+  Context->FileData = PrelinkedContext->Prelinked;
+  Context->FileSize = PrelinkedContext->PrelinkedSize;
 
   return EFI_SUCCESS;
 }

Library/OcAppleKernelLib/KextPatcher.c
@@ -155,7 +155,7 @@ PatcherInitContextFromBuffer (
   // and request PRELINK_KERNEL_IDENTIFIER.
   //
 
-  if (!MachoInitializeContext (&Context->MachContext, Buffer, BufferSize, 0, Is32Bit)) {
+  if (!MachoInitializeContext (&Context->MachContext, Buffer, BufferSize, 0, BufferSize, Is32Bit)) {
     DEBUG ((
       DEBUG_INFO,
       "OCAK: %a-bit patcher init from buffer %p %u has unsupported mach-o\n",
@@ -273,7 +273,7 @@ PatcherGetSymbolAddress (
     Index++;
   }
 
-  *Address = (UINT8 *)MachoGetMachHeader (&Context->MachContext) + Offset;
+  *Address = (UINT8 *)MachoGetFileData (&Context->MachContext) + Offset;
   return EFI_SUCCESS;
 }
 
@@ -289,7 +289,7 @@ PatcherApplyGenericPatch (
   UINT32      ReplaceCount;
 
   Base = (UINT8 *)MachoGetMachHeader (&Context->MachContext);
-  Size = MachoGetFileSize (&Context->MachContext);
+  Size = MachoGetInnerSize (&Context->MachContext);
   if (Patch->Base != NULL) {
     Status = PatcherGetSymbolAddress (Context, Patch->Base, &Base);
     if (EFI_ERROR (Status)) {
@@ -520,7 +520,7 @@ PatcherBlockKext (
   }
 
   MachBase = (UINT8 *)MachoGetMachHeader (&Context->MachContext);
-  MachSize = MachoGetFileSize (&Context->MachContext);
+  MachSize = MachoGetInnerSize (&Context->MachContext);
 
   //
   // Determine offset of kmod within file.

Library/OcAppleKernelLib/Link.c
@@ -1138,7 +1138,7 @@ InternalRelocateAndCopyRelocations (
       }
 
       if (Section32->NumRelocations > 0) {
-        Relocation = (MACH_RELOCATION_INFO *)((UINTN)MachoGetMachHeader32 (&Kext->Context.MachContext) + Section32->RelocationsOffset);
+        Relocation = (MACH_RELOCATION_INFO *)((UINTN)MachoGetFileData (&Kext->Context.MachContext) + Section32->RelocationsOffset);
         for (Index = 0; Index < Section32->NumRelocations; ++Index) {
           NextRelocation = &Relocation[Index + 1];
           //
@@ -1387,7 +1387,7 @@ InternalProcessSymbolPointers (
   IN UINT64                       LoadAddress
   )
 {
-  CONST MACH_HEADER_ANY   *MachHeader;
+  CONST VOID              *FileData;
   UINT32                  MachSize;
   CONST MACH_SECTION_ANY  *Section;
   UINT32                  NumSymbols;
@@ -1434,8 +1434,8 @@ InternalProcessSymbolPointers (
     return FALSE;
   }
 
-  MachHeader = MachoGetMachHeader (MachoContext);
-  ASSERT (MachHeader != NULL);
+  FileData = MachoGetMachHeader (MachoContext);
+  ASSERT (FileData != NULL);
   //
   // Iterate through the indirect symbol table and fill in the section of
   // symbol pointers.  There are three cases:
@@ -1446,14 +1446,14 @@ InternalProcessSymbolPointers (
   //   3) An INDIRECT_SYMBOL_ABS - prepopulated absolute symbols.  No
   //      action is required.
   //
-  Tmp = (VOID *)((UINTN)MachHeader + DySymtab->IndirectSymbolsOffset);
+  Tmp = (VOID *)((UINTN)FileData + DySymtab->IndirectSymbolsOffset);
   if (!OC_TYPE_ALIGNED (UINT32, Tmp)) {
     return FALSE;
   }
 
   SymIndices = (UINT32 *)Tmp + FirstSym;
 
-  IndirectSymPtr = (VOID *)((UINTN)MachHeader + (MachoContext->Is32Bit ? Section->Section32.Offset : Section->Section64.Offset));
+  IndirectSymPtr = (VOID *)((UINTN)FileData + (MachoContext->Is32Bit ? Section->Section32.Offset : Section->Section64.Offset));
   if (MachoContext->Is32Bit ? !OC_TYPE_ALIGNED (UINT32, IndirectSymPtr) : !OC_TYPE_ALIGNED (UINT64, IndirectSymPtr)) {
     return FALSE;
   }
@@ -1512,6 +1512,7 @@ InternalPrelinkKext (
   UINT64                     LinkEditFileOffset;
   UINT64                     LinkEditFileSize;
 
+  VOID                       *FileData;
   MACH_HEADER_ANY            *MachHeader;
   UINT32                     MachSize;
   BOOLEAN                    IsObject32;
@@ -1579,8 +1580,16 @@ InternalPrelinkKext (
   MachoContext    = &Kext->Context.MachContext;
   LinkEditSegment = Kext->LinkEditSegment;
 
+  //
+  // Kexts cannot be contained.
+  //
+  ASSERT ((VOID *)MachoGetMachHeader (MachoContext) == MachoGetFileData (MachoContext));
+  ASSERT (MachoGetInnerSize (MachoContext) == MachoGetFileSize (MachoContext));
+
   MachHeader = MachoGetMachHeader (MachoContext);
   MachSize   = MachoGetFileSize (MachoContext);
+
+  FileData = (VOID *)MachHeader;
 
   IsObject32 = Context->Is32Bit && MachHeader->Header32.FileType == MachHeaderFileTypeObject;
 
@@ -1801,7 +1810,7 @@ InternalPrelinkKext (
     return EFI_LOAD_ERROR;
   }
 
-  KmodInfo = (KMOD_INFO_ANY *)((UINTN)MachHeader + (UINTN)KmodInfoOffset);
+  KmodInfo = (KMOD_INFO_ANY *)((UINTN)FileData + (UINTN)KmodInfoOffset);
 
   FirstSegment = MachoGetNextSegment (MachoContext, NULL);
   if (FirstSegment == NULL) {
@@ -1934,12 +1943,12 @@ InternalPrelinkKext (
     LinkEditSize = (SymbolTableSize + RelocationsSize + StringTableSize);
 
     CopyMem (
-      (VOID *)((UINTN)MachHeader + (UINTN)LinkEditFileOffset),
+      (VOID *)((UINTN)FileData + (UINTN)LinkEditFileOffset),
       LinkEdit,
       LinkEditSize
       );
     ZeroMem (
-      (VOID *)((UINTN)MachHeader + (UINTN)LinkEditFileOffset + LinkEditSize),
+      (VOID *)((UINTN)FileData + (UINTN)LinkEditFileOffset + LinkEditSize),
       (UINTN)(LinkEditFileSize - LinkEditSize)
       );
 
@@ -2000,11 +2009,11 @@ InternalPrelinkKext (
       // Zero out the existing symbol table, and copy our non-undefined symbols back.
       //
       ZeroMem (
-        (VOID *)((UINTN)MachHeader + Symtab->SymbolsOffset),
+        (VOID *)((UINTN)FileData + Symtab->SymbolsOffset),
         SymtabSize
         );
       CopyMem (
-        (VOID *)((UINTN)MachHeader + Symtab->SymbolsOffset),
+        (VOID *)((UINTN)FileData + Symtab->SymbolsOffset),
         SymtabLinkEdit32,
         SymtabSize2
         );
@@ -2137,7 +2146,7 @@ InternalPrelinkKext (
   // Reinitialize the Mach-O context to account for the changed __LINKEDIT
   // segment and file size.
   //
-  if (!MachoInitializeContext (MachoContext, MachHeader, MachSize, MachoContext->ContainerOffset, Context->Is32Bit)) {
+  if (!MachoInitializeContext (MachoContext, MachoContext->FileData, MachSize, 0, MachSize, Context->Is32Bit)) {
     //
     // This should never failed under normal and abnormal conditions.
     //

Library/OcAppleKernelLib/MkextContext.c
@@ -1166,7 +1166,7 @@ MkextReserveKextSize (
 
   if (Executable != NULL) {
     ASSERT (ExecutableSize > 0);
-    if (!MachoInitializeContext (&Context, Executable, ExecutableSize, 0, Is32Bit)) {
+    if (!MachoInitializeContext (&Context, Executable, ExecutableSize, 0, ExecutableSize, Is32Bit)) {
       return EFI_INVALID_PARAMETER;
     }
 

commit 95adcd7be789e8caee7987d6e7626c070157c227

OcAppleKernelLib: Fix ProvideCurrentCpuInfo
Fixes regression introduced by
https://github.com/acidanthera/OpenCorePkg/commit/d916dd65b8cad8647101cddef2f38efd071ebbbd.

Library/OcAppleKernelLib/CpuidPatches.c
@@ -1353,7 +1353,7 @@ PatchProvideCurrentCpuInfo (
   Status  = EFI_SUCCESS;
   Status |= PatchProvideCurrentCpuInfoMSR35h (Patcher, CpuInfo, KernelVersion);
 
-  Start = ((UINT8 *)MachoGetMachHeader (&Patcher->MachContext));
+  Start = ((UINT8 *)MachoGetFileData (&Patcher->MachContext));
 
   //
   // 10.6 and below has variables in __DATA/__data instead of __DATA/__common