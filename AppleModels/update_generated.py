dcd953208a799fc99f5598a7daacc9e75d466e8c
Ivan Cafiero <ivancafiero@gmail.com>
2022-07-13 20:07:48 +0200

    [AppleModels] Fixed formatting

--- AppleModels/update_generated.py
+++ AppleModels/update_generated.py
@@ -198,9 +198,9 @@ def export_db_macserial(db, dbpd, path, year=0):
         print('} AppleModel;\n', file=fh)
         print(f'#define APPLE_MODEL_MAX {len(db)}\n', file=fh)
 
-        print('static PLATFORMDATA ApplePlatformData[] = {', file=fh)
+        print('''static PLATFORMDATA ApplePlatformData[] = {"''', file=fh)
         for info in db:
-            print(f"  {{ '{info['SystemProductName']}', '{info['SystemSerialNumber']}' }},", file=fh)
+            print(f'''  {{ "{info['SystemProductName']}", "{info['SystemSerialNumber']}" }},''', file=fh)
 
         print('};\n', file=fh)
 
