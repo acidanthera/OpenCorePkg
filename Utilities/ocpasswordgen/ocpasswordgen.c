7944e9aa531c4022fe76f190002f6268c4f90e96
Savva Mitrofanov <savvamtr@gmail.com>
2022-08-07 03:53:10 +0600

    ocpasswordgen: Hide password characters on input

--- Utilities/ocpasswordgen/ocpasswordgen.c
+++ Utilities/ocpasswordgen/ocpasswordgen.c
@@ -4,10 +4,47 @@
 **/
 
 #include <stdio.h>
+#ifdef _WIN32
+#include <conio.h>
+#else
+#include <termios.h>
+#include <unistd.h>
+#endif
 
 #include <Library/DebugLib.h>
 #include <Library/OcCryptoLib.h>
 #include <UserPseudoRandom.h>
+
+//
+// Signal Interrupt Control-C symbol
+//
+#define CHAR_END_OF_TEXT 3
+#define CHAR_DELETE      127
+
+#ifndef _WIN32
+/**
+ * getch unix-compatible implementation
+ *
+ * @return integer with ch codenumber
+ */
+int
+getch (
+  void
+  )
+{
+    int Char;
+    struct termios OldTtyAttrs, NewTtyAttrs;
+
+    tcgetattr(STDIN_FILENO, &OldTtyAttrs);
+    NewTtyAttrs = OldTtyAttrs;
+    NewTtyAttrs.c_lflag = NewTtyAttrs.c_lflag & ~(ICANON|ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &NewTtyAttrs);
+    Char = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &OldTtyAttrs);
+
+    return Char;
+}
+#endif
 
 int
 ENTRY_POINT (
@@ -21,12 +58,33 @@ ENTRY_POINT (
   UINT8   Index;
   UINT8   PasswordHash[SHA512_DIGEST_SIZE];
 
-  DEBUG ((DEBUG_ERROR, "Please enter your password: "));
+  DEBUG ((DEBUG_ERROR, "Please enter your password: \n"));
 
   for (PasswordLen = 0; PasswordLen < OC_PASSWORD_MAX_LEN; ++PasswordLen) {
-    Char = getchar ();
-    if ((Char == EOF) || (Char == '\n')) {
+    fflush(stdin);
+    Char = getch ();
+    // Handle interrupt signal for conio
+    if (Char == CHAR_END_OF_TEXT) {
+      exit(EXIT_SUCCESS);
+    }
+    if (Char == EOF || Char == CHAR_LINEFEED) {
       break;
+    }
+    if (Char == CHAR_CARRIAGE_RETURN) {
+      continue;
+    }
+    if (Char == CHAR_BACKSPACE || Char == CHAR_DELETE) {
+      if (PasswordLen > 0) {
+        DEBUG ((DEBUG_ERROR, "\b \b"));
+        PasswordLen--;
+      }
+      continue;
+    }
+
+    if (Char == ' ') {
+      DEBUG ((DEBUG_ERROR, " "));
+    } else {
+      DEBUG ((DEBUG_ERROR, "*"));
     }
 
     Password[PasswordLen] = (UINT8)Char;
@@ -49,7 +107,7 @@ ENTRY_POINT (
     DEBUG ((DEBUG_ERROR, "%02x", PasswordHash[Index]));
   }
 
-  printf (">\nPasswordSalt: <");
+  DEBUG ((DEBUG_ERROR, ">\nPasswordSalt: <"));
   for (Index = 0; Index < sizeof (Salt); ++Index) {
     DEBUG ((DEBUG_ERROR, "%02x", ((UINT8 *)Salt)[Index]));
   }
