111743a74f64a8fc9e870ad8c63670376621af67
vit9696 <vit9696@users.noreply.github.com>
2022-12-25 02:25:07 +0300

    OcAppleSecureBootLib: Fix memory leak

--- Library/OcAppleSecureBootLib/OcAppleSecureBootLib.c
+++ Library/OcAppleSecureBootLib/OcAppleSecureBootLib.c
@@ -615,6 +615,7 @@ InternalGetImg4ByPath (
                   (VOID **)&FileSystem
                   );
   if (EFI_ERROR (Status)) {
+    FreePool (Path);
     return EFI_NO_MEDIA;
   }
 
@@ -653,12 +654,14 @@ InternalGetImg4ByPath (
   }
 
   if (!Result) {
+    FreePool (Path);
     Root->Close (Root);
     return EFI_LOAD_ERROR;
   }
 
   ManifestBuffer = InternalReadFile (Root, Path, &ManifestSize);
 
+  FreePool (Path);
   Root->Close (Root);
 
   if (ManifestBuffer == NULL) {
