97c0246a9c564d2286a574b6232857f8ec78985c
PMheart <17109513+PMheart@users.noreply.github.com>
2023-04-21 11:00:51 +0200

    OcAppleKernelLib: Update 13.0 AICPUPM patch description

--- Library/OcAppleKernelLib/CommonPatches.c
+++ Library/OcAppleKernelLib/CommonPatches.c
@@ -60,6 +60,12 @@ PatchAppleCpuPmCfgLock (
   UINT8  *WalkerEnd;
   UINT8  *WalkerTmp;
 
+  //
+  // NOTE: As of macOS 13.0 AICPUPM kext is removed.
+  // However, legacy version of this kext may be injected and patched,
+  // thus no need to perform system version check here.
+  //
+
   if (Patcher == NULL) {
     DEBUG ((DEBUG_INFO, "OCAK: [OK] Skipping %a on NULL Patcher on kernel version %u\n", __func__, KernelVersion));
     return EFI_NOT_FOUND;
