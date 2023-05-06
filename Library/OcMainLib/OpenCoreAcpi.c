acidanthera/OpenCorePkg

commit f604848c61d7082860fceb23ac54d5e03e620478

OcMainLib: Patch order change (#370)
The current behaviors of how our modification to Booter and Kernel are undocumented. In addition, we should ensure that:

- Deletion is always performed first, as it makes no sense to patch something that will be removed
- Integrated quirks are always applied preceding user patches (to prevent users from messing up the whole patch structure)
- Injection is performed last

Docs/Configuration.tex
@@ -847,10 +847,10 @@ ACPI changes apply globally (to every operating system) with the following effec
 
 \begin{itemize}
 \tightlist
-\item \texttt{Patch} is processed.
 \item \texttt{Delete} is processed.
-\item \texttt{Add} is processed.
 \item \texttt{Quirks} are processed.
+\item \texttt{Patch} is processed.
+\item \texttt{Add} is processed.
 \end{itemize}
 
 Applying the changes globally resolves the problems of incorrect operating system
@@ -1253,6 +1253,13 @@ different firmware types. Some of these features were originally implemented as 
 of \href{https://github.com/acidanthera/AptioFixPkg}{\text{AptioMemoryFix.efi}},
 which is no longer maintained. Refer to the \hyperref[troubleshootingtricks]{Tips and Tricks}
 section for instructions on migration.
+
+Booter changes apply with the following effective order:
+\begin{itemize}
+\tightlist
+\item \texttt{Quirks} are processed.
+\item \texttt{Patch} is processed.
+\end{itemize}
 
 If this is used for the first time on customised firmware, the following requirements
 should be met before starting:
@@ -1947,6 +1954,16 @@ This section allows the application of different kinds of kernelspace modificati
 Apple Kernel (\href{https://opensource.apple.com/source/xnu}{XNU}). The modifications
 currently provide driver (kext) injection, kernel and driver patching, and driver
 blocking.
+
+Kernel and kext changes apply with the following effective order:
+
+\begin{itemize}
+\tightlist
+\item \texttt{Block} is processed.
+\item \texttt{Emulate} and \texttt{Quirks} are processed.
+\item \texttt{Patch} is processed.
+\item \texttt{Add} and \texttt{Force} are processed.
+\end{itemize}
 
 \subsection{Properties}\label{kernelprops}
 

Library/OcMainLib/OpenCoreAcpi.c
@@ -211,15 +211,11 @@ OcLoadAcpiSupport (
     return;
   }
 
+  OcAcpiDeleteTables (Config, &Context);
+
   if (Config->Acpi.Quirks.RebaseRegions) {
     AcpiLoadRegions (&Context);
   }
-
-  OcAcpiPatchTables (Config, &Context);
-
-  OcAcpiDeleteTables (Config, &Context);
-
-  OcAcpiAddTables (Config, Storage, &Context);
 
   if (Config->Acpi.Quirks.FadtEnableReset) {
     AcpiFadtEnableReset (&Context);
@@ -245,6 +241,10 @@ OcLoadAcpiSupport (
   if (Config->Acpi.Quirks.SyncTableIds) {
     AcpiSyncTableIds (&Context);
   }
+
+  OcAcpiPatchTables (Config, &Context);
+
+  OcAcpiAddTables (Config, Storage, &Context);
 
   AcpiApplyContext (&Context);
 

Library/OcAfterBootCompatLib/ServiceOverrides.c
@@ -1262,16 +1262,6 @@ OcStartImage (
     }
   }
 
-  //
-  // Apply customised booter patches.
-  //
-  ApplyBooterPatches (
-    ImageHandle,
-    AppleLoadedImage != NULL,
-    BootCompat->Settings.BooterPatches,
-    BootCompat->Settings.BooterPatchesSize
-    );
-
   if (BootCompat->ServiceState.FwRuntime != NULL) {
     BootCompat->ServiceState.FwRuntime->GetCurrent (&Config);
 
@@ -1315,6 +1305,16 @@ OcStartImage (
                                           &Config
                                           );
   }
+
+  //
+  // Apply customised booter patches.
+  //
+  ApplyBooterPatches (
+    ImageHandle,
+    AppleLoadedImage != NULL,
+    BootCompat->Settings.BooterPatches,
+    BootCompat->Settings.BooterPatchesSize
+    );
 
   Status = BootCompat->ServicePtrs.StartImage (
                                      ImageHandle,

Library/OcMainLib/OpenCoreKernel.c
@@ -785,9 +785,9 @@ OcKernelProcessPrelinked (
   if (!EFI_ERROR (Status)) {
     OcKernelBlockKexts (Config, DarwinVersion, Is32Bit, CacheTypePrelinked, &Context);
 
-    OcKernelInjectKexts (Config, CacheTypePrelinked, &Context, DarwinVersion, Is32Bit, LinkedExpansion, ReservedExeSize);
-
     OcKernelApplyPatches (Config, mOcCpuInfo, DarwinVersion, Is32Bit, CacheTypePrelinked, &Context, NULL, 0);
+
+    OcKernelInjectKexts (Config, CacheTypePrelinked, &Context, DarwinVersion, Is32Bit, LinkedExpansion, ReservedExeSize);
 
     *KernelSize = Context.PrelinkedSize;
 
@@ -818,9 +818,9 @@ OcKernelProcessMkext (
 
   OcKernelBlockKexts (Config, DarwinVersion, Is32Bit, CacheTypeMkext, &Context);
 
-  OcKernelInjectKexts (Config, CacheTypeMkext, &Context, DarwinVersion, Is32Bit, 0, 0);
-
   OcKernelApplyPatches (Config, mOcCpuInfo, DarwinVersion, Is32Bit, CacheTypeMkext, &Context, NULL, 0);
+
+  OcKernelInjectKexts (Config, CacheTypeMkext, &Context, DarwinVersion, Is32Bit, 0, 0);
 
   MkextInjectPatchComplete (&Context);
 
@@ -857,9 +857,9 @@ OcKernelInitCacheless (
 
   OcKernelBlockKexts (Config, DarwinVersion, Is32Bit, CacheTypeCacheless, Context);
 
-  OcKernelInjectKexts (Config, CacheTypeCacheless, Context, DarwinVersion, Is32Bit, 0, 0);
-
   OcKernelApplyPatches (Config, mOcCpuInfo, DarwinVersion, Is32Bit, CacheTypeCacheless, Context, NULL, 0);
+
+  OcKernelInjectKexts (Config, CacheTypeCacheless, Context, DarwinVersion, Is32Bit, 0, 0);
 
   return CachelessContextOverlayExtensionsDir (Context, File);
 }

Library/OcMainLib/OpenCoreKernelPatch.c
@@ -116,118 +116,6 @@ OcKernelApplyPatches (
     }
   }
 
-  for (Index = 0; Index < Config->Kernel.Patch.Count; ++Index) {
-    UserPatch = Config->Kernel.Patch.Values[Index];
-    Target    = OC_BLOB_GET (&UserPatch->Identifier);
-
-    if (!UserPatch->Enabled || ((AsciiStrCmp (Target, "kernel") == 0) != IsKernelPatch)) {
-      continue;
-    }
-
-    Comment   = OC_BLOB_GET (&UserPatch->Comment);
-    Arch      = OC_BLOB_GET (&UserPatch->Arch);
-    MaxKernel = OcParseDarwinVersion (OC_BLOB_GET (&UserPatch->MaxKernel));
-    MinKernel = OcParseDarwinVersion (OC_BLOB_GET (&UserPatch->MinKernel));
-
-    if (AsciiStrCmp (Arch, Is32Bit ? "x86_64" : "i386") == 0) {
-      DEBUG ((
-        DEBUG_INFO,
-        "OC: %a patcher skips %a (%a) patch at %u due to arch %a != %a\n",
-        PRINT_KERNEL_CACHE_TYPE (CacheType),
-        Target,
-        Comment,
-        Index,
-        Arch,
-        Is32Bit ? "i386" : "x86_64"
-        ));
-      continue;
-    }
-
-    if (!OcMatchDarwinVersion (DarwinVersion, MinKernel, MaxKernel)) {
-      DEBUG ((
-        DEBUG_INFO,
-        "OC: %a patcher skips %a (%a) patch at %u due to version %u <= %u <= %u\n",
-        PRINT_KERNEL_CACHE_TYPE (CacheType),
-        Target,
-        Comment,
-        Index,
-        MinKernel,
-        DarwinVersion,
-        MaxKernel
-        ));
-      continue;
-    }
-
-    //
-    // Ignore patch if:
-    // - There is nothing to replace.
-    // - We have neither symbolic base, nor find data.
-    // - Find and replace mismatch in size.
-    // - Mask and ReplaceMask mismatch in size when are available.
-    //
-    if (  (UserPatch->Replace.Size == 0)
-       || ((OC_BLOB_GET (&UserPatch->Base)[0] == '\0') && (UserPatch->Find.Size != UserPatch->Replace.Size))
-       || ((UserPatch->Mask.Size > 0) && (UserPatch->Find.Size != UserPatch->Mask.Size))
-       || ((UserPatch->ReplaceMask.Size > 0) && (UserPatch->Find.Size != UserPatch->ReplaceMask.Size)))
-    {
-      DEBUG ((DEBUG_ERROR, "OC: Kernel patch %u for %a (%a) is borked\n", Index, Target, Comment));
-      continue;
-    }
-
-    ZeroMem (&Patch, sizeof (Patch));
-
-    if (OC_BLOB_GET (&UserPatch->Comment)[0] != '\0') {
-      Patch.Comment = OC_BLOB_GET (&UserPatch->Comment);
-    }
-
-    if (OC_BLOB_GET (&UserPatch->Base)[0] != '\0') {
-      Patch.Base = OC_BLOB_GET (&UserPatch->Base);
-    }
-
-    if (UserPatch->Find.Size > 0) {
-      Patch.Find = OC_BLOB_GET (&UserPatch->Find);
-    }
-
-    Patch.Replace = OC_BLOB_GET (&UserPatch->Replace);
-
-    if (UserPatch->Mask.Size > 0) {
-      Patch.Mask = OC_BLOB_GET (&UserPatch->Mask);
-    }
-
-    if (UserPatch->ReplaceMask.Size > 0) {
-      Patch.ReplaceMask = OC_BLOB_GET (&UserPatch->ReplaceMask);
-    }
-
-    Patch.Size  = UserPatch->Replace.Size;
-    Patch.Count = UserPatch->Count;
-    Patch.Skip  = UserPatch->Skip;
-    Patch.Limit = UserPatch->Limit;
-
-    if (IsKernelPatch) {
-      Status = PatcherApplyGenericPatch (&KernelPatcher, &Patch);
-    } else {
-      if (CacheType == CacheTypeCacheless) {
-        Status = CachelessContextAddPatch (Context, Target, &Patch);
-      } else if (CacheType == CacheTypeMkext) {
-        Status = MkextContextApplyPatch (Context, Target, &Patch);
-      } else if (CacheType == CacheTypePrelinked) {
-        Status = PrelinkedContextApplyPatch (Context, Target, &Patch);
-      } else {
-        Status = EFI_UNSUPPORTED;
-      }
-    }
-
-    DEBUG ((
-      EFI_ERROR (Status) ? DEBUG_WARN : DEBUG_INFO,
-      "OC: %a patcher result %u for %a (%a) - %r\n",
-      PRINT_KERNEL_CACHE_TYPE (CacheType),
-      Index,
-      Target,
-      Comment,
-      Status
-      ));
-  }
-
   //
   // Handle Quirks/Emulate here...
   //
@@ -382,6 +270,118 @@ OcKernelApplyPatches (
     if (Config->Kernel.Quirks.ProvideCurrentCpuInfo) {
       PatchProvideCurrentCpuInfo (&KernelPatcher, CpuInfo, DarwinVersion);
     }
+  }
+
+  for (Index = 0; Index < Config->Kernel.Patch.Count; ++Index) {
+    UserPatch = Config->Kernel.Patch.Values[Index];
+    Target    = OC_BLOB_GET (&UserPatch->Identifier);
+
+    if (!UserPatch->Enabled || ((AsciiStrCmp (Target, "kernel") == 0) != IsKernelPatch)) {
+      continue;
+    }
+
+    Comment   = OC_BLOB_GET (&UserPatch->Comment);
+    Arch      = OC_BLOB_GET (&UserPatch->Arch);
+    MaxKernel = OcParseDarwinVersion (OC_BLOB_GET (&UserPatch->MaxKernel));
+    MinKernel = OcParseDarwinVersion (OC_BLOB_GET (&UserPatch->MinKernel));
+
+    if (AsciiStrCmp (Arch, Is32Bit ? "x86_64" : "i386") == 0) {
+      DEBUG ((
+        DEBUG_INFO,
+        "OC: %a patcher skips %a (%a) patch at %u due to arch %a != %a\n",
+        PRINT_KERNEL_CACHE_TYPE (CacheType),
+        Target,
+        Comment,
+        Index,
+        Arch,
+        Is32Bit ? "i386" : "x86_64"
+        ));
+      continue;
+    }
+
+    if (!OcMatchDarwinVersion (DarwinVersion, MinKernel, MaxKernel)) {
+      DEBUG ((
+        DEBUG_INFO,
+        "OC: %a patcher skips %a (%a) patch at %u due to version %u <= %u <= %u\n",
+        PRINT_KERNEL_CACHE_TYPE (CacheType),
+        Target,
+        Comment,
+        Index,
+        MinKernel,
+        DarwinVersion,
+        MaxKernel
+        ));
+      continue;
+    }
+
+    //
+    // Ignore patch if:
+    // - There is nothing to replace.
+    // - We have neither symbolic base, nor find data.
+    // - Find and replace mismatch in size.
+    // - Mask and ReplaceMask mismatch in size when are available.
+    //
+    if (  (UserPatch->Replace.Size == 0)
+       || ((OC_BLOB_GET (&UserPatch->Base)[0] == '\0') && (UserPatch->Find.Size != UserPatch->Replace.Size))
+       || ((UserPatch->Mask.Size > 0) && (UserPatch->Find.Size != UserPatch->Mask.Size))
+       || ((UserPatch->ReplaceMask.Size > 0) && (UserPatch->Find.Size != UserPatch->ReplaceMask.Size)))
+    {
+      DEBUG ((DEBUG_ERROR, "OC: Kernel patch %u for %a (%a) is borked\n", Index, Target, Comment));
+      continue;
+    }
+
+    ZeroMem (&Patch, sizeof (Patch));
+
+    if (OC_BLOB_GET (&UserPatch->Comment)[0] != '\0') {
+      Patch.Comment = OC_BLOB_GET (&UserPatch->Comment);
+    }
+
+    if (OC_BLOB_GET (&UserPatch->Base)[0] != '\0') {
+      Patch.Base = OC_BLOB_GET (&UserPatch->Base);
+    }
+
+    if (UserPatch->Find.Size > 0) {
+      Patch.Find = OC_BLOB_GET (&UserPatch->Find);
+    }
+
+    Patch.Replace = OC_BLOB_GET (&UserPatch->Replace);
+
+    if (UserPatch->Mask.Size > 0) {
+      Patch.Mask = OC_BLOB_GET (&UserPatch->Mask);
+    }
+
+    if (UserPatch->ReplaceMask.Size > 0) {
+      Patch.ReplaceMask = OC_BLOB_GET (&UserPatch->ReplaceMask);
+    }
+
+    Patch.Size  = UserPatch->Replace.Size;
+    Patch.Count = UserPatch->Count;
+    Patch.Skip  = UserPatch->Skip;
+    Patch.Limit = UserPatch->Limit;
+
+    if (IsKernelPatch) {
+      Status = PatcherApplyGenericPatch (&KernelPatcher, &Patch);
+    } else {
+      if (CacheType == CacheTypeCacheless) {
+        Status = CachelessContextAddPatch (Context, Target, &Patch);
+      } else if (CacheType == CacheTypeMkext) {
+        Status = MkextContextApplyPatch (Context, Target, &Patch);
+      } else if (CacheType == CacheTypePrelinked) {
+        Status = PrelinkedContextApplyPatch (Context, Target, &Patch);
+      } else {
+        Status = EFI_UNSUPPORTED;
+      }
+    }
+
+    DEBUG ((
+      EFI_ERROR (Status) ? DEBUG_WARN : DEBUG_INFO,
+      "OC: %a patcher result %u for %a (%a) - %r\n",
+      PRINT_KERNEL_CACHE_TYPE (CacheType),
+      Index,
+      Target,
+      Comment,
+      Status
+      ));
   }
 }
 

commit c6f75e10a765f8487fc22235d139e7bcd613b40f

OcMainLib/OpenCoreAcpi: Fix orders for `RebaseRegions` and `SyncTableIds`
This fixes regression introduced by https://github.com/acidanthera/OpenCorePkg/commit/f604848c61d7082860fceb23ac54d5e03e620478.

Docs/Configuration.tex
@@ -857,9 +857,10 @@ ACPI changes apply globally (to every operating system) with the following effec
 \begin{itemize}
 \tightlist
 \item \texttt{Delete} is processed.
-\item \texttt{Quirks} are processed.
+\item \texttt{Quirks} are processed, with the exception of \texttt{RebaseRegions} and \texttt{SyncTableIds}.
 \item \texttt{Patch} is processed.
 \item \texttt{Add} is processed.
+\item \texttt{RebaseRegions} and \texttt{SyncTableIds} are processed, if requested.
 \end{itemize}
 
 Applying the changes globally resolves the problems of incorrect operating system

Library/OcMainLib/OpenCoreAcpi.c
@@ -243,21 +243,21 @@ OcLoadAcpiSupport (
   //
   AcpiHandleHardwareSignature (&Context, Config->Acpi.Quirks.ResetHwSig);
 
-  if (Config->Acpi.Quirks.RebaseRegions) {
-    AcpiRelocateRegions (&Context);
-  }
-
   if (Config->Acpi.Quirks.NormalizeHeaders) {
     AcpiNormalizeHeaders (&Context);
-  }
-
-  if (Config->Acpi.Quirks.SyncTableIds) {
-    AcpiSyncTableIds (&Context);
   }
 
   OcAcpiPatchTables (Config, &Context);
 
   OcAcpiAddTables (Config, Storage, &Context);
+
+  if (Config->Acpi.Quirks.RebaseRegions) {
+    AcpiRelocateRegions (&Context);
+  }
+
+  if (Config->Acpi.Quirks.SyncTableIds) {
+    AcpiSyncTableIds (&Context);
+  }
 
   AcpiApplyContext (&Context);
 