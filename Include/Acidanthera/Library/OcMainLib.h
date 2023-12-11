3df90c7315ff36f85d508c0fbf78ee1e2a5c8a99
Andrey1970AppleLife <ua6lpo@rambler.ru>
2023-12-11 23:41:54 +0300

    Bump version

--- Include/Acidanthera/Library/OcMainLib.h
+++ Include/Acidanthera/Library/OcMainLib.h
@@ -30,7 +30,7 @@
   OpenCore version reported to log and NVRAM.
   OPEN_CORE_VERSION must follow X.Y.Z format, where X.Y.Z are single digits.
 **/
-#define OPEN_CORE_VERSION  "0.9.7"
+#define OPEN_CORE_VERSION  "0.9.8"
 
 /**
   OpenCore build type reported to log and NVRAM.
