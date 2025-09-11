5a996a4436d44180b813ed8311521eec88eff4ec
vit9696 <vit9696@users.noreply.github.com>
2025-09-11 11:57:33 +0300

    Utilities/macrecovery: Add option to customise build-image image format

--- Utilities/macrecovery/build-image.sh
+++ Utilities/macrecovery/build-image.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -e
 
-rm -rf Recovery.RO.dmg Recovery.RO.raw Recovery.dmg.sparseimage
+if [ -z "$1" ]; then
+  FORMAT="raw"
+else
+  FORMAT="${1}"
+fi
+
+rm -rf Recovery.RO.dmg Recovery.raw "Recovery.${FORMAT}" Recovery.dmg.sparseimage
 hdiutil create -size 800m -layout "UNIVERSAL HD" -type SPARSE -o Recovery.dmg
 newDevice=$(hdiutil attach -nomount Recovery.dmg.sparseimage | head -n 1 | awk  '{print $1}')
 echo newdevice "$newDevice"
@@ -14,5 +20,5 @@ diskutil umount disk"${N}"s1
 hdiutil detach "$newDevice"
 hdiutil convert -format UDZO Recovery.dmg.sparseimage -o Recovery.RO.dmg
 rm Recovery.dmg.sparseimage
-qemu-img convert -f dmg -O raw Recovery.RO.dmg Recovery.raw
+qemu-img convert -f dmg -O "${FORMAT}" Recovery.RO.dmg "Recovery.${FORMAT}"
 rm Recovery.RO.dmg
